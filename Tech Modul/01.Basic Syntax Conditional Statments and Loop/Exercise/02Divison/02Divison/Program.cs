30b10f8b5a73c036bf47b23ad842dc750f210735
Ventsislav <venci.velkov@gmail.com>
2020-01-15 14:28:27 +0200

    add

--- Tech Modul/01.Basic Syntax Conditional Statments and Loop/Exercise/02Divison/02Divison/Program.cs
+++ Tech Modul/01.Basic Syntax Conditional Statments and Loop/Exercise/02Divison/02Divison/Program.cs
@@ -6,7 +6,37 @@ namespace _02Divison
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number = int.Parse(Console.ReadLine());
+            int divisible = 0;
+
+            if (number % 2 == 0)
+            {
+                divisible = 2;
+            }
+            if (number % 3 == 0)
+            {
+                divisible = 3;
+            }
+            if (number % 6 == 0)
+            {
+                divisible = 6;
+            }
+            if (number % 7 == 0)
+            {
+                divisible = 7;
+            }
+            if (number % 10 == 0)
+            {
+                divisible = 10;
+            }
+            if (divisible == 0)
+            {
+                Console.WriteLine("Not divisible");
+            }
+            else
+            {
+                Console.WriteLine($"The number is divisible by {divisible}");
+            }
         }
     }
 }
