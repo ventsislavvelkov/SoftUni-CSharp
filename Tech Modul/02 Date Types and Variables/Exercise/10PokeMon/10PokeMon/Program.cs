6a09a6685a2866a1f8c10e49dfcc2cdf5ab192e4
Ventsislav <venci.velkov@gmail.com>
2020-01-17 18:11:55 +0200

    add

--- Tech Modul/02 Date Types and Variables/Exercise/10PokeMon/10PokeMon/Program.cs
+++ Tech Modul/02 Date Types and Variables/Exercise/10PokeMon/10PokeMon/Program.cs
@@ -6,7 +6,31 @@ namespace _10PokeMon
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int power = int.Parse(Console.ReadLine());
+            int distance = int.Parse(Console.ReadLine());
+            int exhaustion = int.Parse(Console.ReadLine());
+
+            int countOfTarget = 0;
+            int firstN = power;
+
+            while (true)
+            {
+                if (firstN * 0.50 == power && exhaustion >0)
+                {
+                    power /= exhaustion;
+                }
+                else
+                {
+                    power -= distance;
+                    countOfTarget++;
+                }
+                if (power < distance)
+                {
+                    Console.WriteLine(power);
+                    Console.WriteLine(countOfTarget);
+                    break;
+                }
+            }
         }
     }
 }
---
+++ Tech Modul/02 Date Types and Variables/Exercise/11Snowballs/11Snowballs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _11Snowballs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
