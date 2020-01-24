88354cf06bbea314dcb523e3bcae4a44448ef4c7
Ventsislav <venci.velkov@gmail.com>
2020-01-24 23:54:43 +0200

    Associative Array Exer 01-02

---
+++ Tech Modul/07 Associative Arrays/Exercise/01CountCharsInAString/01CountCharsInAString/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace _01CountCharsInAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            var letters = new Dictionary<char, int>();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                char letter = word[i];
+
+                if (letter != ' ')
+                {
+                    if (!letters.ContainsKey(letter))
+                    {
+                        letters.Add(letter, 1);
+                    }
+                    else
+                    {
+                        letters[letter]++;
+                    }
+                }
+
+            }
+
+            foreach (var item in letters)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
---
+++ Tech Modul/07 Associative Arrays/Exercise/02AMinerTask/02AMinerTask/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace _02AMinerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var resource = new Dictionary<string, int>();
+
+            while (input != "stop")
+            {
+                int quantity = int.Parse(Console.ReadLine()); ;
+
+                if (!resource.ContainsKey(input))
+                {
+                    resource.Add(input, quantity);
+
+                }
+                else
+                {
+                    resource[input] += quantity;
+                }
+
+
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var item in resource)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+
+        }
+    }
+}
