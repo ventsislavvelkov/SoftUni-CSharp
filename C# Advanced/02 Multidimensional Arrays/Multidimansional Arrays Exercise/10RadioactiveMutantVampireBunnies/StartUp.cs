0a873e5da67c200efa56164bfca35c8281e9b4dc
Ventsislav <venci.velkov@gmail.com>
2020-01-30 09:18:30 +0200

    add

--- C# Advanced/02 Multidimensional Arrays/Multidimansional Arrays Exercise/10RadioactiveMutantVampireBunnies/StartUp.cs
+++ C# Advanced/02 Multidimensional Arrays/Multidimansional Arrays Exercise/10RadioactiveMutantVampireBunnies/StartUp.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 
 namespace _10RadioactiveMutantVampireBunnies
 {
@@ -7,47 +9,161 @@ namespace _10RadioactiveMutantVampireBunnies
     {
         static void Main()
         {
-            var n = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var dimentions = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
 
-            var rows = n[0];
-            var cols = n[1];
+            var rows = dimentions[0];
+            var cols = dimentions[1];
 
-            var matrix = ReadMatrix(rows, cols);
-
-            var command = Console.ReadLine();
-
-            
-        }
-
-
-        static char[,] ReadMatrix(int rows, int cols)
-        {
             var matrix = new char[rows, cols];
+            var playerRow = 0;
+            var playerCol = 0;
 
             for (int row = 0; row < rows; row++)
             {
-                var currentValue = Console.ReadLine();
+                var rowValues = Console.ReadLine().ToCharArray();
 
                 for (int col = 0; col < cols; col++)
                 {
-                    matrix[row, col] = currentValue[col];
+                    matrix[row, col] = rowValues[col];
+
+                    if (matrix[row, col] == 'P')
+                    {
+                        playerRow = row;
+                        playerCol = col;
+                    }
                 }
             }
 
-            return matrix;
+            var directons = Console.ReadLine().ToCharArray();
+            var isWon = false;
+            var isDead = false;
+
+            foreach (var direction in directons)
+            {
+                var playerNewRow = playerRow;
+                var playerNewCol = playerCol;
+
+                switch (direction)
+                {
+                    case 'U':
+                        playerNewRow--;
+                        break;
+                    case 'D':
+                        playerNewRow++;
+                        break;
+                    case 'L':
+                        playerNewCol--;
+                        break;
+                    case 'R':
+                        playerNewCol++;
+                        break;
+                }
+
+                isWon = IsWon(matrix, playerNewRow, playerNewCol);
+
+                if (!isWon)
+                {
+                    isDead = IsSymbol(matrix, 'B', playerNewRow, playerNewCol);
+
+                    if (!isDead)
+                    {
+                        matrix[playerNewRow, playerNewCol] = 'P';
+                    }
+
+                    matrix[playerRow, playerCol] = '.';
+                    playerRow = playerNewRow;
+                    playerCol = playerNewCol;
+                }
+                else
+                {
+                    matrix[playerRow, playerCol] = '.';
+                }
+
+                var bunniesCordinates = new List<int>();
+
+                for (int row = 0; row < rows; row++)
+                {
+                    for (int col = 0; col < cols; col++)
+                    {
+                        if (matrix[row, col] == 'B')
+                        {
+                            bunniesCordinates.Add(row);
+                            bunniesCordinates.Add(col);
+                        }
+                    }
+                }
+
+                for (int i = 0; i < bunniesCordinates.Count; i += 2)
+                {
+                    var bunnyRow = bunniesCordinates[i];
+                    var bunnyCol = bunniesCordinates[i + 1];
+
+                    SpreadBunny(matrix, bunnyRow, bunnyCol);
+                }
+
+                isDead = IsSymbol(matrix, 'B', playerRow, playerCol);
+
+                if (isWon || isDead)
+                {
+                    break;
+                }
+            }
+
+            PrintMatrix(matrix);
+            if (isWon)
+            {
+                Console.WriteLine($"won: {playerRow} {playerCol}");
+            }
+            else
+            {
+                Console.WriteLine($"dead: {playerRow} {playerCol}");
+            }
         }
 
-        static bool IsValidCell(char[,] matrix, int row, int col)
+        static void SpreadBunny(char[,] matrix, int row, int col)
         {
-            var isValid = false;
+            if (row - 1 >= 0)
+            {
+                matrix[row - 1, col] = 'B';
+            }
+            if (row + 1 < matrix.GetLength(0))
+            {
+                matrix[row + 1, col] = 'B';
+            }
+            if (col - 1 >= 0)
+            {
+                matrix[row, col - 1] = 'B';
+            }
+            if (col + 1 < matrix.GetLength(1))
+            {
+                matrix[row, col + 1] = 'B';
+            }
+        }
+
+        static bool IsSymbol(char[,] matrix, char symbol, int row, int col)
+        {
+            var IsSymbol = false;
+
+            if (matrix[row, col] == symbol)
+            {
+                IsSymbol = true;
+            }
+
+            return IsSymbol;
+        }
+
+        static bool IsWon(char[,] matrix, int row, int col)
+        {
+            var isWon = true;
 
             if (row >= 0 && row < matrix.GetLength(0) &&
                 col >= 0 && col < matrix.GetLength(1))
             {
-                isValid = true;
+                isWon = false;
             }
 
-            return isValid;
+            return isWon;
+
         }
 
         static void PrintMatrix(char[,] matrix)
