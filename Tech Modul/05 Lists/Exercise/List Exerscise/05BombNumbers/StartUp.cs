8e874de00a1157c1bb0156e69be553f358faa271
Ventsislav <venci.velkov@gmail.com>
2020-02-05 14:08:13 +0200

    add

---
+++ Tech Modul/05 Lists/Exercise/List Exerscise/05BombNumbers/StartUp.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Dynamic;
+using System.Linq;
+
+namespace _05BombNumbers
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)
+                .ToList();
+
+            var specialBomb = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)
+                .ToArray();
+            var specialNumber = specialBomb[0];
+            var lenght = specialBomb[1];
+            var count = lenght;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (specialNumber == numbers[i])
+                {
+                    while (count > 0)
+                    {
+                        if (i - count >= 0 && i - count < numbers.Count)
+                        {
+                            numbers.RemoveAt(i - count);
+   
+                            i--;
+                        }
+
+                        count--;
+                    }
+
+                    count = lenght;
+
+                    while (count > 0)
+                    {
+                        if (i + count >= 0 && i + count < numbers.Count)
+                        {
+                            numbers.RemoveAt(i + count);
+                        }
+
+                        count--;
+                    }
+
+                    numbers.RemoveAt(i);
+                }
+
+                count = lenght;
+            }
+
+            var sum = 0;
+
+            foreach (var num in numbers)
+            {
+                sum += num;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Tech Modul/05 Lists/Exercise/List Exerscise/06CardsGame/StartUp.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06CardsGame
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstDeck = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> secondDeck = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            int sum = 0;
+
+            while (firstDeck.Count > 0 && secondDeck.Count > 0)
+            {
+                for (int i = 0; i < Math.Min(firstDeck.Count, secondDeck.Count); i++)
+                {
+                    if (firstDeck[i] == secondDeck[i])
+                    {
+                        firstDeck.RemoveAt(i);
+                        secondDeck.RemoveAt(i);
+                    }
+                    else if (firstDeck[i] > secondDeck[i])
+                    {
+                        firstDeck.Add(firstDeck[i]);
+                        firstDeck.Add(secondDeck[i]);
+                        secondDeck.RemoveAt(i);
+                        firstDeck.RemoveAt(i);
+                    }
+                    else
+                    {
+                        secondDeck.Add(secondDeck[i]);
+                        secondDeck.Add(firstDeck[i]);
+                        firstDeck.RemoveAt(i);
+                        secondDeck.RemoveAt(i);
+                    }
+                    if (firstDeck.Count == 0 || secondDeck.Count == 0) break;
+                }
+            }
+            if (firstDeck.Count > 0)
+            {
+                for (int i = 0; i < firstDeck.Count; i++)
+                {
+                    sum += firstDeck[i];
+                }
+                Console.WriteLine($"First player wins! Sum: {sum}");
+            }
+            if (secondDeck.Count > 0)
+            {
+                for (int i = 0; i < secondDeck.Count; i++)
+                {
+                    sum += secondDeck[i];
+                }
+                Console.WriteLine($"Second player wins! Sum: {sum}");
+            }
+        }
+    }
+}
