0b2a471c375c6430501d85926c1107fe2c979dc1
Ventsislav <venci.velkov@gmail.com>
2020-02-11 20:50:48 +0200

    add

---
+++ Associative Arrays Exercise/01CountCharsInAString/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace _01CountCharsInAString
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var word = Console.ReadLine();
+
+            var letters = new Dictionary<char, int>();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                var letter = word[i];
+
+                if (letter != ' ')
+                {
+                    if (!letters.ContainsKey(letter))
+                    {
+                        letters.Add(letter, 1);
+                    }
+                    else
+                    {
+                        letters[letter]++;
+                    }
+                }
+
+            }
+
+            foreach (var item in letters)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
