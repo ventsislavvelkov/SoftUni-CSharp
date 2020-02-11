783c3e91a41a809a9fbd836a0d22ff589fc929dd
Ventsislav <venci.velkov@gmail.com>
2020-02-11 22:11:29 +0200

    add

---
+++ Tech Modul/07 Associative Arrays/Exercise/Associative Arrays Exercise/04Orders/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04Orders
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var orders = new Dictionary<string, double[]>();
+
+            while (true)
+            {
+                var input = Console.ReadLine().Split();
+                var product = input[0];
+
+                if (product == "buy")
+                {
+                    break;
+                }
+                else
+                {
+                    var price = double.Parse(input[1]);
+                    var quantity = int.Parse(input[2]);
+
+                    if (!orders.ContainsKey(product))
+                    {
+                        orders[product] = new double[2];
+                    }
+
+                    orders[product][0] = price;
+                    orders[product][1] += quantity;
+                }
+            }
+
+
+            foreach (var kvp in orders)
+            {
+                Console.WriteLine($"{kvp.Key} -> {(kvp.Value[0] * kvp.Value[1]):f2}");
+            }
+        }
+    }
+}
