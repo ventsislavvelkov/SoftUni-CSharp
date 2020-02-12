127b2ca169037e6de2336ef86d47ec085f1e1a16
Ventsislav <venci.velkov@gmail.com>
2020-02-12 17:33:50 +0200

    add

---
+++ Tech Modul/07 Associative Arrays/Exercise/Associative Arrays Exercise/05SoftUniParking/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05SoftUniParking
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var commandNumbers = int.Parse(Console.ReadLine());
+            var softUniParking = new Dictionary<string,string>();
+
+            for (int i = 0; i < commandNumbers; i++)
+            {
+                
+            }
+        }
+    }
+}
