9106e36d9a5be9f190f3790f2957d8bc0dd2ddf2
Ventsislav <venci.velkov@gmail.com>
2020-02-12 21:48:37 +0200

    add

--- Tech Modul/07 Associative Arrays/Exercise/Associative Arrays Exercise/05SoftUniParking/StartUp.cs
+++ Tech Modul/07 Associative Arrays/Exercise/Associative Arrays Exercise/05SoftUniParking/StartUp.cs
@@ -9,10 +9,39 @@ namespace _05SoftUniParking
         {
             var commandNumbers = int.Parse(Console.ReadLine());
             var softUniParking = new Dictionary<string,string>();
+          
 
             for (int i = 0; i < commandNumbers; i++)
             {
-                
+                var input = Console.ReadLine().Split();
+                var command = input[0];
+                var name = input[1];
+                var plateNumber = input[2];
+
+                if (command == "register")
+                {
+                    if (!softUniParking.ContainsKey(name))
+                    {
+                        softUniParking[name] = plateNumber;
+                        Console.WriteLine($"{name} registered {plateNumber} successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {plateNumber}");
+                    }
+                }
+                else
+                {
+                    if (softUniParking.ContainsKey(name))
+                    {
+                        softUniParking[name] = plateNumber;
+                        Console.WriteLine($"{name} unregistered successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: user {name} not found");
+                    }
+                }
             }
         }
     }
