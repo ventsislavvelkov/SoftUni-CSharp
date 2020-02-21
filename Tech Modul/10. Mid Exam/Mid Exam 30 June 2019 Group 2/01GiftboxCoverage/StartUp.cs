8275ecabfdf85940cad1c12010eb777d4bddf520
Ventsislav <venci.velkov@gmail.com>
2020-02-22 00:36:09 +0200

    Giftbox Coverage

--- Tech Modul/10. Mid Exam/Mid Exam 30 June 2019 Group 2/01GiftboxCoverage/StartUp.cs
+++ Tech Modul/10. Mid Exam/Mid Exam 30 June 2019 Group 2/01GiftboxCoverage/StartUp.cs
@@ -6,7 +6,30 @@ namespace _01GiftboxCoverage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var sizeOfSide = double.Parse(Console.ReadLine());
+            var numbersOfSheetOfPaper = int.Parse(Console.ReadLine());
+            var areaOfSingleSheet = double.Parse(Console.ReadLine());
+
+            var totalArea = sizeOfSide * sizeOfSide * 6;
+
+            var areaCovered = 0.0;
+
+            for (int i = 1; i <= numbersOfSheetOfPaper; i++)
+            {
+                if (i % 3 != 0)
+                {
+                    areaCovered += areaOfSingleSheet;
+                }
+                else
+                {
+                    areaCovered += areaOfSingleSheet * 0.25;
+                }
+                
+            }
+
+            var percentCover = (areaCovered / totalArea) * 100;
+
+            Console.WriteLine($"You can cover {percentCover:F2}% of the box.");
         }
     }
 }
