e408b6a3cab6695234413f0d70f022da1a8a312c
Ventsislav <venci.velkov@gmail.com>
2020-02-23 14:27:37 +0200

    Courses

--- Tech Modul/07 Associative Arrays/Exercise/Associative Arrays Exercise/06Courses/StartUp.cs
+++ Tech Modul/07 Associative Arrays/Exercise/Associative Arrays Exercise/06Courses/StartUp.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
+using Microsoft.VisualBasic;
 
 namespace _06Courses
 {
@@ -7,7 +10,34 @@ namespace _06Courses
     {
         static void Main(string[] args)
         {
-            var name = Console.ReadLine();
+            var coursAndStudents = new Dictionary<string, List<string>>();
+
+            while (true)
+            {
+                var input = Console.ReadLine().Split(" : ");
+                var cours = input[0];
+
+                if (cours == "end")
+                {
+                    break;
+                }
+                else
+                {
+                    var studentName = input[1];
+
+                    if (!coursAndStudents.ContainsKey(cours))
+                    {
+                        coursAndStudents.Add(cours, new List<string>());
+                    }
+                    coursAndStudents[cours].Add(studentName);
+                }
+            }
+
+            foreach (var course in coursAndStudents.OrderByDescending(x=>x.Value.Count))
+            {
+                Console.WriteLine($"{course.Key}: {course.Value.Count}");
+                Console.WriteLine($"-- {string.Join(Environment.NewLine+"-- ", course.Value.OrderBy(x=>x))}");
+            }
         }
     }
-}
+} 
