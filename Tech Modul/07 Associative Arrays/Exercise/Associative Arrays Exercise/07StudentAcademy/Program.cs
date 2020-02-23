da4c7148d96d9e78731dd9f5ccb9afa21ed8a1f5
Ventsislav <venci.velkov@gmail.com>
2020-02-23 15:08:32 +0200

    Student Academy

--- Tech Modul/07 Associative Arrays/Exercise/Associative Arrays Exercise/07StudentAcademy/Program.cs
+++ Tech Modul/07 Associative Arrays/Exercise/Associative Arrays Exercise/07StudentAcademy/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Reflection;
+using System.Xml;
 
 namespace _07StudentAcademy
 {
@@ -6,7 +12,41 @@ namespace _07StudentAcademy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var studentAcademy = new Dictionary<string, List<double>>();
+
+            var count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                var studentName = Console.ReadLine();
+                var grade = double.Parse(Console.ReadLine());
+
+                if (!studentAcademy.ContainsKey(studentName))
+                {
+                    studentAcademy.Add(studentName, new List<double>());
+                }
+
+                studentAcademy[studentName].Add(grade);
+
+            }
+
+            var resultList = new Dictionary<string,double>();
+
+            foreach (var student in studentAcademy)
+            {
+                var avegageGrade = student.Value.Average();
+                if (avegageGrade >= 4.50)
+                {
+                    resultList.Add(student.Key, avegageGrade);
+                }
+            }
+
+            foreach (var student in resultList.OrderByDescending(x => x.Value))
+            {
+                Console.WriteLine($"{student.Key} -> {student.Value:F2}");
+            }
+
         }
     }
 }
+
