ebf445bd232e6f9a91df6c3c56f158ebf6868125
Ventsislav <venci.velkov@gmail.com>
2020-02-28 00:07:07 +0200

    add

---
+++ Tech Modul/08 Text Processing/Text Processing - Exercise/05MultiplyBigNumber/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace _05MultiplyBigNumber
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var firstNumber = Console.ReadLine();
+            var multiplier = int.Parse(Console.ReadLine());
+
+            var builder = new StringBuilder();
+
+            var onMind = 0;
+
+            for (int i = firstNumber.Length-1; i >= 0 ; i--)
+            {
+                var currentNumber = int.Parse(firstNumber[i].ToString());
+
+                var result = currentNumber * multiplier + onMind;
+
+                builder.Append(result % 10);
+                
+                onMind = result / 10;
+            }
+
+            if (onMind != 0 )
+            {
+                builder.Append(onMind);
+            }
+
+            var resultNumer =string.Join("", builder.ToString().Reverse()).TrimStart('0');
+
+            if (resultNumer == string.Empty)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                Console.WriteLine(resultNumer);
+            }
+        }
+    }
+}
