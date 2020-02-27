52bfc76c8d7638cbe119789c2cd94fd794f4d152
Ventsislav <venci.velkov@gmail.com>
2020-02-28 00:06:03 +0200

    add

---
+++ Tech Modul/08 Text Processing/Text Processing - Exercise/04CaesarCipher/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04CaesarCipher
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var strings = Console.ReadLine();
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                var chars = strings[i];
+
+                var convert = chars + 3;
+
+                chars = Convert.ToChar(convert);
+
+                Console.Write(chars);
+            }
+        }
+    }
+}
