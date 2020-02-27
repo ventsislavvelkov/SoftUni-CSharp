70ea17cb9c9b36e389f2f2e262193e1b5e3d10d8
Ventsislav <venci.velkov@gmail.com>
2020-02-28 00:55:03 +0200

    Add

--- Tech Modul/07 Associative Arrays/Exercise/Associative Arrays Exercise/09ForceBook/StartUp.cs
+++ Tech Modul/07 Associative Arrays/Exercise/Associative Arrays Exercise/09ForceBook/StartUp.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _09ForceBook
 {
@@ -6,7 +8,97 @@ namespace _09ForceBook
     {
         static void Main(string[] args)
         {
-           
+            var database = new Dictionary<string, List<string>>();
+            var forceUsers = new List<string>();
+
+            var input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "Lumpawaroo")
+            {
+                if (input.Contains(" | "))
+                {
+                    var tokens = input.Split(" | ");
+                    var forceSide = tokens[0];
+                    var forceUser = tokens[1];
+
+                    if (!forceUsers.Contains(forceUser))
+                    {
+                        if (!database.ContainsKey(forceSide))
+                        {
+                            database.Add(forceSide, new List<string>() { forceUser });
+                        }
+                        else
+                        {
+                            database[forceSide].Add(forceUser);
+                        }
+                    }
+
+                    forceUsers.Add(forceUser);
+                }
+                else
+                {
+                    var tokens = input.Split(" -> ");
+                    var forceUser = tokens[0];
+                    var forceSide = tokens[1];
+
+                    // change sides
+                    if (forceUsers.Contains(forceUser))
+                    {
+                        var userCurrentSide = string.Empty;
+
+                        foreach (var key in database.Keys)
+                        {
+                            if (database[key].Contains(forceUser))
+                            {
+                                userCurrentSide = key;
+                                break;
+                            }
+                        }
+
+                        if (!database.ContainsKey(forceSide))
+                        {
+                            database[forceSide] = new List<string>();
+                        }
+                        database[userCurrentSide].Remove(forceUser);
+                        database[forceSide].Add(forceUser);
+                    }
+                    else
+                    {
+                        if (!database.ContainsKey(forceSide))
+                        {
+                            database.Add(forceSide, new List<string>() { forceUser });
+                        }
+                        else
+                        {
+                            database[forceSide].Add(forceUser);
+                        }
+                    }
+
+                    Console.WriteLine($"{forceUser} joins the {forceSide} side!");
+
+                    if (!forceUsers.Contains(forceUser))
+                    {
+                        forceUsers.Add(forceUser);
+                    }
+                }
+            }
+
+
+            foreach (var item in database.OrderByDescending(x => x.Value.Count).ThenBy(x => x.Key))
+            {
+                var forceSide = item.Key;
+                var members = item.Value;
+
+                if (members.Count > 0)
+                {
+                    Console.WriteLine($"Side: {forceSide}, Members: {members.Count}");
+
+                    foreach (var member in members.OrderBy(x => x))
+                    {
+                        Console.WriteLine($"! {member}");
+                    }
+                }
+            }
 
         }
     }
---
+++ Tech Modul/07 Associative Arrays/Exercise/Associative Arrays Exercise/10SoftUniExamResults/StartUp.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10SoftUniExamResults
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var studentsAndPoints = new Dictionary<string, int>();
+            var submissions = new Dictionary<string, int>();
+
+            var input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "exam finished")
+            {
+                var tokens = input.Split("-");
+                var userName = tokens[0];
+
+                if (tokens[1] == "banned")
+                {
+                    studentsAndPoints.Remove(userName);
+                }
+                else
+                {
+                    var language = tokens[1];
+                    var points = int.Parse(tokens[2]);
+
+                    if (!studentsAndPoints.ContainsKey(userName))
+                    {
+                        studentsAndPoints[userName] = points;
+                    }
+                    else if (points > studentsAndPoints[userName])
+                    {
+                        studentsAndPoints[userName] = points;
+                    }
+
+                    if (!submissions.ContainsKey(language))
+                    {
+                        submissions[language] = 1;
+                        // submissions[language] = 0;
+                    }
+                    // without else part we can directly iterate it
+                    else
+                    {
+                        submissions[language]++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Results:");
+
+            foreach (var kvp in studentsAndPoints.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{kvp.Key} | {kvp.Value}");
+            }
+
+            Console.WriteLine("Submissions:");
+
+            foreach (var kvp in submissions.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value}");
+            }
+        }
+    }
+}
