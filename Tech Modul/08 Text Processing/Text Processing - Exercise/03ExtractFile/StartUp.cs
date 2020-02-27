8857902bbb6f1ea6e7c9f6d45ecc5d7dd66e8658
Ventsislav <venci.velkov@gmail.com>
2020-02-28 00:05:40 +0200

    add

---
+++ Tech Modul/08 Text Processing/Text Processing - Exercise/03ExtractFile/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace _03ExtractFile
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var file = Console.ReadLine().Split(@"\");
+
+            var lastFile = file[file.Length - 1];
+
+            var lastNameLenght = lastFile.IndexOf('.');
+
+            var name = lastFile.Substring(0, lastNameLenght);
+            var extension = lastFile.Substring(lastNameLenght + 1, (lastFile.Length-1 - lastNameLenght));
+
+            Console.WriteLine($"File name: {name}");
+            Console.WriteLine($"File extension: {extension}");
+
+        }
+    }
+}
