0d5ed58109ce48e906e9898ade43ce1b536293e0
Ventsislav <venci.velkov@gmail.com>
2020-02-28 00:05:16 +0200

    add

---
+++ Tech Modul/08 Text Processing/Text Processing - Exercise/02CharacterMultiplier/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _02CharacterMultiplier
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+           var strings = Console.ReadLine().Split();
+
+           var firstStr = strings[0];
+           var secondStr = strings[1];
+
+           var minStr = string.Empty;
+           var maxStr = string.Empty;
+
+           if (firstStr.Length > secondStr.Length)
+           {
+               minStr = secondStr;
+               maxStr = firstStr;
+           }
+           else
+           {
+               minStr = firstStr;
+               maxStr = secondStr;
+           }
+
+           var totalSum = 0; 
+
+           for (int i = 0; i < minStr.Length; i++)
+           {
+               totalSum += minStr[i] * maxStr[i];
+           }
+
+           for (int i = minStr.Length; i < maxStr.Length; i++)
+           {
+               totalSum += maxStr[i];
+           }
+
+           Console.WriteLine(totalSum);
+
+
+        }
+    }
+}
