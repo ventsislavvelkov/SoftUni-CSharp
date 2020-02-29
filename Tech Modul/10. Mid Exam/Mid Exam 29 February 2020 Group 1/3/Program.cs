3dd690bd1af73538062244ef51db0c9edaa3f0b0
Ventsislav Velkov <venci.velkov@gmail.com>
2020-02-29 13:05:34 +0200

    Inventory

---
+++ Tech Modul/10. Mid Exam/Mid Exam 29 February 2020 Group 1/3/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03Inventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inventory = Console.ReadLine().Split(", ").ToList();
+            var isExists = false;
+
+            while (true)
+            {
+                var input = Console.ReadLine().Split(" - ");
+
+                var command = input[0];
+
+                if (command == "Craft!")
+                {
+                    break;
+                }
+
+                var ithem = input[1];
+
+                if (command == "Collect")
+                {
+                    isExists = IsExists(inventory, ithem);
+
+                    if (!isExists)
+                    {
+                        inventory.Add(ithem);
+                    }
+                }
+                else if (command == "Drop")
+                {
+                    isExists = IsExists(inventory, ithem);
+
+                    if (isExists)
+                    {
+                        inventory.Remove(ithem);
+                    }
+                }
+                else if (command == "Combine Items")
+                {
+                    var combine = ithem.Split(":").ToArray();
+                    var firstIthem = combine[0].ToString();
+                    var secondIthem = combine[1].ToString();
+                    isExists = IsExists(inventory, firstIthem);
+
+                    if (isExists)
+                    {
+                        var index = inventory.IndexOf(firstIthem);
+                        inventory.Insert(index+1, secondIthem);
+                    }
+                }
+                else if (command == "Renew")
+                {
+                    isExists = IsExists(inventory, ithem);
+
+                    if (isExists)
+                    {
+                        inventory.Remove(ithem);
+                        inventory.Add(ithem);
+
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", inventory));
+        }
+
+        private static bool IsExists(List<string> inventory, string ithem)
+        {
+            var isExists = false;
+
+            if (inventory.Contains(ithem))
+            {
+                isExists = true;
+            }
+
+            return isExists;
+        }
+    }
+}
