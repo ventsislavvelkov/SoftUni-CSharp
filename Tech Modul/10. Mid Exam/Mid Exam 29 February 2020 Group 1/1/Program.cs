2d640379400b2590b53a661eb61d2cd32d4dd84f
Ventsislav Velkov <venci.velkov@gmail.com>
2020-02-29 13:04:23 +0200

    Bonus Scoring System

---
+++ Tech Modul/10. Mid Exam/Mid Exam 29 February 2020 Group 1/1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _01BonusScoringSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = int.Parse(Console.ReadLine());
+            var countOfLectures = int.Parse(Console.ReadLine());
+            var bonus = int.Parse(Console.ReadLine());
+
+            var maxBonus = 0.0;
+            var studentAttendance = 0.0;
+
+            for (int i = 1; i <= students; i++)
+            {
+                var attendance = double.Parse(Console.ReadLine());
+
+                var currnentbonus = Math.Ceiling((attendance / countOfLectures) * (5 + bonus));
+
+                if (maxBonus <= currnentbonus)
+                {
+                    maxBonus = currnentbonus;
+                    studentAttendance = attendance;
+                }
+
+            }
+
+            Console.WriteLine($"Max Bonus: {(maxBonus)}.");
+            Console.WriteLine($"The student has attended {studentAttendance} lectures.");
+        }
+    }
+}
