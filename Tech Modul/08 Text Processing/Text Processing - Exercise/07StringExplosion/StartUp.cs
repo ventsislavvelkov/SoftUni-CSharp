77eebc43f1bd9b35e305007fcdc4d40855ca7e70
Ventsislav <venci.velkov@gmail.com>
2020-03-01 19:21:38 +0200

    StringExplosion

---
+++ Tech Modul/08 Text Processing/Text Processing - Exercise/07StringExplosion/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace _07StringExplosion
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var  explosion = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (explosion > 0 && input[i] != '>')
+                {
+                    input = input.Remove(i, 1);     
+                    explosion--;        
+                    i--;                          
+                }
+                else if (input[i] == '>')
+                {
+                    explosion += int.Parse(input[i + 1].ToString());     
+                }
+            }
+
+            Console.WriteLine(input);
+        }
+    }
+}
