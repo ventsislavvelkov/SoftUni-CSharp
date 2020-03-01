bcfa6e9d1f95a11b665fba07f7a3f0dfbd7d7b7f
Ventsislav <venci.velkov@gmail.com>
2020-03-01 19:21:05 +0200

    ReplaceRepeatingChar

---
+++ Tech Modul/08 Text Processing/Text Processing - Exercise/06ReplaceRepeatingChars/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace _06ReplaceRepeatingChars
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var strings = Console.ReadLine();
+
+            var builder = new StringBuilder();
+            builder.Append(strings[0]);
+            var lastChar = 0;
+
+            for (int i = 1; i < strings.Length; i++)
+            {
+                var currentChar = strings[i];
+
+                if (currentChar != strings[lastChar])
+                {
+                    builder.Append(currentChar);
+                }
+
+                lastChar = i;
+            }
+
+            Console.WriteLine(builder);
+        }
+    }
+}
