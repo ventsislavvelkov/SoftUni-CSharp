09bdc90abe4a8f0e2fa7121be1fcfc6ef5b9b81e
Ventsislav <venci.velkov@gmail.com>
2020-03-02 20:19:23 +0200

    CountUpperCase

---
+++ C# Advanced/04 Functional Programing/Exercise/Lab Functional Programming/3CountUppercaseWords/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _3CountUppercaseWords
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> checker = n => n[0] == n.ToUpper()[0];
+
+            var strings = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Where(checker)
+                .ToArray();
+
+            foreach (var word in strings)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
