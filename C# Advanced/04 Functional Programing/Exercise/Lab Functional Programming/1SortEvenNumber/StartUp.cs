6f463b1cf34aebed7a9bb0f12ac600bf11801bbb
Ventsislav <venci.velkov@gmail.com>
2020-03-02 19:59:00 +0200

    SortEvenNumber

---
+++ C# Advanced/04 Functional Programing/Exercise/Lab Functional Programming/1SortEvenNumber/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _1SortEvenNumber
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var number = Console.ReadLine().Split(",")
+                .Select(int.Parse)
+                .Where(n => n % 2 == 0)
+                .OrderBy(n => n)
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ", number));
+
+        }
+    }
+}
