eb8dc3df8d65e6b37c71833c8c73b8bbad2ecac5
Ventsislav <venci.velkov@gmail.com>
2020-03-02 20:03:22 +0200

    SumNumbers

---
+++ C# Advanced/04 Functional Programing/Exercise/Lab Functional Programming/2SumNumbers/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _2SumNumbers
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(",")
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
