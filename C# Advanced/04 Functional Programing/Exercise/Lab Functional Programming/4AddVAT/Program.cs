0de2a50fb97a290973d0dbcdcc41090f973bf820
Ventsislav <venci.velkov@gmail.com>
2020-03-02 20:19:42 +0200

    AddVAT

---
+++ C# Advanced/04 Functional Programing/Exercise/Lab Functional Programming/4AddVAT/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _4AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var price = Console.ReadLine()
+                .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .Select(n => n * 1.2)
+                .ToArray();
+
+            foreach (var prices in price)
+            {
+                Console.WriteLine($"{prices:F2}");
+            }
+        }
+    }
+}
