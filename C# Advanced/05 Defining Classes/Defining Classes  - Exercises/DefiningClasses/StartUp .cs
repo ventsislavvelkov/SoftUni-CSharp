a2b9b09122065c82af52cab15ef52378d78af92c
Ventsislav <venci.velkov@gmail.com>
2020-03-07 00:39:57 +0200

    add

---
+++ C# Advanced/05 Defining Classes/Defining Classes  - Exercises/DefiningClasses/StartUp .cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var name = "Pesho";
+            var age = 24;
+
+            Person person = new Person()
+            {
+                Name = name,
+                Age = age
+            };
+
+
+           Console.WriteLine($"{person.Name} -> {person.Age}");
+        }
+    }
+}
