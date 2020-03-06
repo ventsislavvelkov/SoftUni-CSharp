36614a120f6110737c9a8194c232a90354b258cf
Ventsislav <venci.velkov@gmail.com>
2020-03-07 01:09:05 +0200

    Constructors

--- C# Advanced/05 Defining Classes/Defining Classes  - Exercises/DefiningClasses/Person.cs
+++ C# Advanced/05 Defining Classes/Defining Classes  - Exercises/DefiningClasses/Person.cs
@@ -7,7 +7,21 @@
 
         public Person()
         {
+            this.name = "No name";
+            this.age = 1;
 
+        }
+
+        public Person(int age)
+        {
+            this.age = age;
+            this.Name = "No name";
+        }
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
         }
 
         public string Name
