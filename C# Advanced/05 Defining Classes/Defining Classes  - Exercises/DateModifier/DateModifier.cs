837f7279477e808c28fa8f3a310151fd67ac2fb9
Ventsislav <venci.velkov@gmail.com>
2020-03-08 00:05:36 +0200

    add

---
+++ C# Advanced/05 Defining Classes/Defining Classes  - Exercises/DateModifier/DateModifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DateModifier
+{
+    public class DateModifier
+    {
+        private DateTime startDate;
+        private DateTime endDate;
+
+        public int GetDifferenceInDays(string firstDate, string secondDate)
+        {
+            this.startDate = DateTime.Parse(firstDate);
+            this.endDate = DateTime.Parse(secondDate);
+
+            var result = (int)((this.startDate - this.endDate).TotalDays);
+            return Math.Abs(result);
+        }
+    }
+}
