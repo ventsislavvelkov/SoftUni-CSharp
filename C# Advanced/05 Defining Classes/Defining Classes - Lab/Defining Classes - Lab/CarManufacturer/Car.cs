5dec3d5b8cdefd294ac6c55fa3c87fd0fa3f03a7
Ventsislav <venci.velkov@gmail.com>
2020-03-08 00:05:08 +0200

    Add

--- C# Advanced/05 Defining Classes/Defining Classes - Lab/Defining Classes - Lab/CarManufacturer/Car.cs
+++ C# Advanced/05 Defining Classes/Defining Classes - Lab/Defining Classes - Lab/CarManufacturer/Car.cs
@@ -1,18 +1,77 @@
-﻿namespace CarManufacturer
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+
+namespace CarManufacturer
 {
     public class Car
     {
         private string make;
         private string model;
         private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
 
-        public string Make { get; set; }
-        public string Model { get; set; }
-        public int Year { get; set; }
 
-        public Car()
+        public string Make
         {
+            get { return this.make; }
 
+            set { this.make = value; }
+        }
+
+        public string Model
+        {
+            get { return this.model; }
+
+            set { this.model = value; }
+        }
+
+        public int Year
+        {
+            get { return this.year; }
+
+            set { this.year = value; }
+        }
+
+        public double FuelQuantity
+        {
+            get { return this.fuelQuantity; }
+
+            set { this.fuelQuantity = value; }
+        }
+
+        public double FuelConsumption
+        {
+            get { return this.fuelConsumption; }
+
+            set { this.fuelConsumption = value; }
+        }
+
+        public void Drive(double distance)
+        {
+            double consumation = distance * this.FuelConsumption / 100;
+
+            if (this.FuelQuantity > consumation)
+            {
+                this.FuelQuantity -= consumation;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"Make: {this.Make}");
+            sb.AppendLine($"Model: {this.Model}");
+            sb.AppendLine($"Year: {this.Year}");
+            sb.AppendLine($"Fuel: {this.FuelQuantity:F2}L");
+
+            return sb.ToString();
         }
     }
 }
