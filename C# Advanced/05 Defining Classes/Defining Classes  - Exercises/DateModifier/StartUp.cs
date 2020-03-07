dccd53d5d216e58be47906499ad409bf1d06d58e
Ventsislav <venci.velkov@gmail.com>
2020-03-08 00:05:53 +0200

    add

---
+++ C# Advanced/05 Defining Classes/Defining Classes  - Exercises/DateModifier/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DateModifier
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var firstDate = Console.ReadLine();
+            var secondDate = Console.ReadLine();
+
+            DateModifier  date = new DateModifier();
+
+            Console.WriteLine(date.GetDifferenceInDays(firstDate,secondDate));
+        }
+    }
+}
