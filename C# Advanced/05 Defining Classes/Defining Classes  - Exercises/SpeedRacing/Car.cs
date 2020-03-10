aabf34141b864edbfbda6395b90ef748ed6fcc41
Ventsislav <venci.velkov@gmail.com>
2020-03-11 01:06:06 +0200

    Speed Racing

---
+++ C# Advanced/05 Defining Classes/Defining Classes  - Exercises/SpeedRacing/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpeedRacing
+{
+    public class Car
+    {
+        private string model;
+        private double fuelAmount;
+        private double fuelConsumptionPerKilometer;
+        private double travelledDistance;
+
+        public string Model
+        {
+            get { return this.model; }
+            set { this.model = value; }
+        }
+
+        public double FuelAmount
+        {
+            get { return this.fuelAmount; }
+            set { this.fuelAmount = value; }
+        }
+
+        public double FuelConsumptionPerKilometer
+        {
+            get { return this.fuelConsumptionPerKilometer; }
+            set { this.fuelConsumptionPerKilometer = value; }
+
+        }
+
+        public double TravelledDistance
+        {
+            get { return this.travelledDistance; }
+            set { this.travelledDistance = value; }
+        }
+
+
+    }
+}
