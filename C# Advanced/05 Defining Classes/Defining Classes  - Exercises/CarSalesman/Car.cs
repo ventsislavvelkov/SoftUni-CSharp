a9e818e257816ff6bdd7827f644baaef41af4ed6
Ventsislav Velkov <venci.velkov@gmail.com>
2020-03-15 19:34:49 +0200

    carSalesman

--- C# Advanced/05 Defining Classes/Defining Classes  - Exercises/CarSalesman/Car.cs
+++ C# Advanced/05 Defining Classes/Defining Classes  - Exercises/CarSalesman/Car.cs
@@ -4,7 +4,35 @@ using System.Text;
 
 namespace CarSalesman
 {
-    class Car
+    public class Car
     {
+        private string model;
+        private string engine;
+        private int weight;
+        private string color;
+
+        public string Model
+        {
+            get => this.model;
+            set => this.model = value;
+        }
+
+        public string Engine
+        {
+            get => this.engine;
+            set => this.engine = value;
+        }
+
+        public int Weight
+        {
+            get => this.weight;
+            set => this.weight = value;
+        }
+
+        public string Color
+        {
+            get => this.color;
+            set => this.color = value;
+        }
     }
 }
--- C# Advanced/05 Defining Classes/Defining Classes  - Exercises/CarSalesman/Engine.cs
+++ C# Advanced/05 Defining Classes/Defining Classes  - Exercises/CarSalesman/Engine.cs
@@ -4,7 +4,35 @@ using System.Text;
 
 namespace CarSalesman
 {
-    class Engine
+    public class Engine
     {
+        private string model;
+        private int power;
+        private int displacement;
+        private int efficiency;
+
+        public string Model
+        {
+            get => this.model;
+            set => this.model = value;
+        }
+
+        public int Power
+        {
+            get => this.power;
+            set => this.power = value;
+        }
+
+        public int Displacement
+        {
+            get => this.displacement;
+            set => this.displacement = value;
+        }
+
+        public int Efficiency
+        {
+            get => this.efficiency;
+            set => this.efficiency = value;
+        }
     }
 }
