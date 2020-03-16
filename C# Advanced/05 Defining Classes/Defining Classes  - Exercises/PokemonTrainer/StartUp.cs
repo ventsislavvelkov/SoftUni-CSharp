0f0f2d152a95ceee4cf2e994866b1808aab0b8b5
Ventsislav Velkov <venci.velkov@gmail.com>
2020-03-16 22:09:37 +0200

    PokemonTraniner

---
+++ C# Advanced/05 Defining Classes/Defining Classes  - Exercises/PokemonTrainer/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PokemonTrainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
