1729265bfdc806501c78e590e91c73253e73774c
Ventsislav Velkov <venci.velkov@gmail.com>
2020-03-18 00:34:07 +0200

    Matche Phone Number

---
+++ Tech Modul/09 Regular Expressions/Lab/P02MatchPhoneNumber/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace P02MatchPhoneNumber
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var regex = @"(?<!\d)[+]359(\s{1}|-)2\1\d{3}\1\d{4}\b";
+
+            var phones = Console.ReadLine();
+
+            var phoneMatches = Regex.Matches(phones, regex);
+
+            var matchedPhones = phoneMatches.Cast<Match>()
+                .Select(a => a.Value.Trim())
+                .ToArray();
+            Console.WriteLine(string.Join(", ", matchedPhones));
+        }
+    }
+}
