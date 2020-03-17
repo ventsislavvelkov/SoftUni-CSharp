6b5d7ef437c8ba0d1c69c1c684198fd3375c4ee3
Ventsislav Velkov <venci.velkov@gmail.com>
2020-03-18 01:09:41 +0200

    Match Dates

---
+++ Tech Modul/09 Regular Expressions/Lab/p03MatchDates/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace p03MatchDates
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            var regex = @"\b(?<day>\d{2})([-.\/])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b";
+            var matches = Console.ReadLine();
+
+            var matchesDate = Regex.Matches(matches, regex);
+
+            foreach (Match date in matchesDate)
+            {
+                var day = date.Groups["day"].Value;
+                var month = date.Groups["month"].Value;
+                var year = date.Groups["year"].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
