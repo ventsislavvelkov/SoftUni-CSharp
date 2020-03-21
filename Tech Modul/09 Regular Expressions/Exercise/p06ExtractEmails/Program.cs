3dfd39f594e1bb976afc3a1aa3f1305dcc1a12d9
Ventsislav Velkov <venci.velkov@gmail.com>
2020-03-21 20:06:04 +0200

    Homework

---
+++ Tech Modul/09 Regular Expressions/Exercise/p06ExtractEmails/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace p06ExtractEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
