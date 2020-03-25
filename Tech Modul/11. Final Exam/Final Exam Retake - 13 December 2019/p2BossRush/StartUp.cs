fe4b778e8905f04d85fa5fe6c35792f92b56f2df
Ventsislav Velkov <venci.velkov@gmail.com>
2020-03-25 22:21:52 +0200

    FinalExam

--- Tech Modul/11. Final Exam/Final Exam Retake - 13 December 2019/p2BossRush/StartUp.cs
+++ Tech Modul/11. Final Exam/Final Exam Retake - 13 December 2019/p2BossRush/StartUp.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace p2BossRush
 {
@@ -6,7 +7,28 @@ namespace p2BossRush
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var n = int.Parse(Console.ReadLine());
+            var pattern = @"\|(?<boss>[A-Z]{4,})\|:#(?<title>[A-Z]?[a-z]+ [A-Z]?[a-z]+)#";
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine();
+                var regex = Regex.Match(input, pattern);
+
+                if (regex.Success)
+                {
+                    var name = regex.Groups["boss"].Value;
+                    var title = regex.Groups["title"].Value;
+
+                    Console.WriteLine($"{name}, The {title}");
+                    Console.WriteLine($">>Strength: {name.Length}");
+                    Console.WriteLine($">>Armour: {title.Length}");
+                }
+                else
+                {
+                    Console.WriteLine("Access denied!");
+                }
+            }
         }
     }
 }
