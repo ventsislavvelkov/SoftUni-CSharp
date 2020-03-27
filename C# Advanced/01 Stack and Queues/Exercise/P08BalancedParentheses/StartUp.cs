681286544640d8094bc986cca37d1deeb6415c67
Ventsislav Velkov <venci.velkov@gmail.com>
2020-03-27 21:01:14 +0200

    Homeworks

--- C# Advanced/01 Stack and Queues/Exercise/P08BalancedParentheses/StartUp.cs
+++ C# Advanced/01 Stack and Queues/Exercise/P08BalancedParentheses/StartUp.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace P08BalancedParentheses
 {
@@ -6,7 +8,54 @@ namespace P08BalancedParentheses
     {
         static void Main(string[] args)
         {
-            
+            var stackOfParanteses = new Stack<char>();
+            var input = Console.ReadLine().ToCharArray();
+
+            var openParaneteses = new char[] { '(', '{', '[' };
+
+            bool isValid = true;
+
+            foreach (var ch in input)
+            {
+                if (openParaneteses.Contains(ch))
+                {
+                    stackOfParanteses.Push(ch);
+                    continue;
+                }
+
+                if (stackOfParanteses.Count == 0)
+                {
+                    isValid = false;
+                    break;
+                }
+
+                if (stackOfParanteses.Peek() == '(' && ch == ')')
+                {
+                    stackOfParanteses.Pop();
+                }
+                else if (stackOfParanteses.Peek() == '[' && ch == ']')
+                {
+                    stackOfParanteses.Pop();
+                }
+                else if (stackOfParanteses.Peek() == '{' && ch == '}')
+                {
+                    stackOfParanteses.Pop();
+                }
+                else
+                {
+                    isValid = false;
+                    break;
+                }
+            }
+
+            if (isValid)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
         }
     }
 }
