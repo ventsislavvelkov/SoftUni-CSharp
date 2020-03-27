0a4cac9a16551a15f277f1b80e8c6c59ce923ff1
Ventsislav Velkov <venci.velkov@gmail.com>
2020-03-27 22:54:32 +0200

    Homeworks

---
+++ C# Advanced/01 Stack and Queues/Exercise/P09SimpleTextEditor/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+
+namespace P09SimpleTextEditor
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var stack = new Stack<string>();
+            var text = new StringBuilder();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                var command = input[0];
+
+                if (command == "1")
+                {
+                    var someString = input[1];
+                    stack.Push(text.ToString());
+                    text.Append(someString);
+                }
+                else if (command == "2")
+                {
+                    var count = int.Parse(input[1]);
+                    var startIndex = text.Length - count;
+                    stack.Push(text.ToString());
+                    text.Remove(startIndex, count);
+
+                }
+                else if (command == "3")
+                {
+                    var index = int.Parse(input[1]);
+                    Console.WriteLine(text[index-1]);
+                   
+                }
+                else if (command == "4" && stack.Count > 0)
+                {
+                    text.Clear();
+                    text.Append(stack.Pop());
+                }
+            }
+        }
+    }
+}
