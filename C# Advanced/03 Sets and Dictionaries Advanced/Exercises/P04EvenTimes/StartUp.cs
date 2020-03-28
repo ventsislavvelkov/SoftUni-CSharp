3c25e6a780b2801615d52c312e820cd3943d346d
Ventsislav Velkov <venci.velkov@gmail.com>
2020-03-29 00:57:09 +0200

    Homeworks

---
+++ C# Advanced/03 Sets and Dictionaries Advanced/Exercises/P04EvenTimes/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04EvenTimes
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var numberOfIntegers = int.Parse(Console.ReadLine());
+            var numbers = new Dictionary<int, int>();
+
+            for (int i = 0; i < numberOfIntegers; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+
+                if (!numbers.ContainsKey(currentNumber))
+                {
+                    numbers.Add(currentNumber, 0);
+                }
+
+                numbers[currentNumber]++;
+            }
+
+            var evenNumber = numbers.SingleOrDefault(x => x.Value % 2 == 0).Key;
+
+            Console.WriteLine(evenNumber);
+        }
+    }
+}
