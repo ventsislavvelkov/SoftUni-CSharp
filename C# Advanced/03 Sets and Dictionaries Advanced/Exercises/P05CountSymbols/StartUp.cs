53dab4decdcf2e51ba3932a5751d832548713220
Ventsislav Velkov <venci.velkov@gmail.com>
2020-03-29 00:57:30 +0200

    Homeworks

---
+++ C# Advanced/03 Sets and Dictionaries Advanced/Exercises/P05CountSymbols/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05CountSymbols
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine().ToCharArray();
+            var letterCounts = new Dictionary<char,int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                var currentCh = text[i];
+                if (!letterCounts.ContainsKey(currentCh))
+                {
+                    letterCounts.Add(currentCh,0);
+                }
+
+                letterCounts[currentCh]++;
+            }
+
+            foreach (var (key,value) in letterCounts.OrderBy(x=>x.Key))
+            {
+                Console.WriteLine($"{key}: {value} time/s");
+            }
+        }
+    }
+}
