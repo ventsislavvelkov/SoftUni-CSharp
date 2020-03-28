34b5a18b72f19aadaacfa67fa87ebead9df960eb
Ventsislav Velkov <venci.velkov@gmail.com>
2020-03-28 22:23:02 +0200

    Homeworks

---
+++ C# Advanced/03 Sets and Dictionaries Advanced/Exercises/P01UniqueUsernames/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P01UniqueUsernames
+{
+    class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var numberOfUsernames = int.Parse(Console.ReadLine());
+            var usernames = new HashSet<string>();
+
+            var username = string.Empty;
+            
+            for (int i = 0; i < numberOfUsernames; i++)
+            {
+                username = Console.ReadLine();
+                usernames.Add(username);
+            }
+
+            foreach (var name in usernames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
