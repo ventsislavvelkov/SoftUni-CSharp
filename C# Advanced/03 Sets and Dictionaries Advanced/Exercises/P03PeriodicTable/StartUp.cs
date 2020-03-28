46ea66fc760b92afb1acd5ef1c0f4d65f0defdee
Ventsislav Velkov <venci.velkov@gmail.com>
2020-03-29 00:56:44 +0200

    Homeworks

---
+++ C# Advanced/03 Sets and Dictionaries Advanced/Exercises/P03PeriodicTable/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03PeriodicTable
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var unique = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    var currentElement = input[j];
+
+                    if (!unique.Contains(currentElement))
+                    {
+                        unique.Add(currentElement);
+                    }
+                }
+
+            }
+
+            foreach (var uni in unique.OrderBy(x=>x))
+            {
+                Console.Write(uni +" ");
+            }
+        }
+    }
+}
