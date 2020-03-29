dabe57787a8ad329727d6af1c453706b58fde2ce
Ventsislav Velkov <venci.velkov@gmail.com>
2020-03-29 22:43:43 +0300

    FinalExam

---
+++ Tech Modul/11. Final Exam/Final Exam - 07 December 2019 Group 1/P03InboxManager/StartUp.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace P03InboxManager
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var inboxManager = new Dictionary<string, List<string>>();
+            var input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "Statistics")
+            {
+                var tokkens = input.Split("->",StringSplitOptions.RemoveEmptyEntries).ToArray();
+                var command = tokkens[0];
+                var username = tokkens[1];
+
+                if (command == "Add")
+                {
+                    if (!inboxManager.ContainsKey(username))
+                    {
+                        inboxManager.Add(username, new List<string>());
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{username} is already registered");
+                    }
+                }
+                else if (command == "Send")
+                {
+                    var message = tokkens[2];
+
+                    if (inboxManager.ContainsKey(username))
+                    {
+                        inboxManager[username].Add(message);
+                    }
+                }
+                else if (command == "Delete")
+                {
+                    if (inboxManager.ContainsKey(username))
+                    {
+                        inboxManager.Remove(username);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{username} not found!");
+                    }
+                }
+            }
+
+            Console.WriteLine($"Users count: {inboxManager.Keys.Count}");
+
+
+            //foreach (var (key,value) in inboxManager
+            //            .OrderByDescending(x=>x.Value.Count)
+            //            .ThenBy(x=>x.Key))
+            //{
+            //    Console.WriteLine(key);
+            //    Console.WriteLine($" - {string.Join(Environment.NewLine + " - ", value)}");
+            //}
+
+            foreach (var kvp in inboxManager
+                .OrderByDescending(x => x.Value.Count)
+                .ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{kvp.Key}");
+
+                foreach (var item in kvp.Value)
+                {
+                    Console.WriteLine($" - {item}");
+                }
+            }
+        }
+    }
+}
