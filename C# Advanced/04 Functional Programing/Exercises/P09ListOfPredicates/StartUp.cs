d42f5503303dc9351db46d3ef199f2703029bdd4
Ventsislav Velkov <venci.velkov@gmail.com>
2020-03-30 18:50:40 +0300

    Homeworks

---
+++ C# Advanced/04 Functional Programing/Exercises/P09ListOfPredicates/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P09ListOfPredicates
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var endOfRange = int.Parse(Console.ReadLine());
+            var divider = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var resultNumbers = Enumerable.Range(1, endOfRange).ToList();
+
+            var currentDivider = 0;
+
+            for (int i = 0; i < divider.Length; i++)
+            {
+                currentDivider = divider[i];
+
+                Predicate<int> filterNumbers = number => number % currentDivider == 0;
+
+                resultNumbers = resultNumbers
+                    .Where(x => filterNumbers(x))
+                    .ToList();
+            }
+
+            Action<List<int>> printNumbers = numbers =>
+                Console.WriteLine(string.Join(" ", numbers));
+
+            printNumbers(resultNumbers);
+        }
+    }
+}
