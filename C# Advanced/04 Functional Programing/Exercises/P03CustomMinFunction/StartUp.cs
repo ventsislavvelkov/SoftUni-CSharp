a2435b91e941a49801a2774e1a5e0c290729dbc3
Ventsislav Velkov <venci.velkov@gmail.com>
2020-03-30 18:47:06 +0300

    Homeworks

---
+++ C# Advanced/04 Functional Programing/Exercises/P03CustomMinFunction/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace P03CustomMinFunction
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var inputNumbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int[], int> minNumbers = numbers =>
+            {
+                int minValue = int.MaxValue;
+
+                foreach (var number in numbers)
+                {
+                    if (number < minValue)
+                    {
+                        minValue = number;
+                    }
+                }
+
+                return minValue;
+            };
+
+            Action<int> printMinValue = minNumber =>
+                Console.WriteLine(minNumber);
+
+            printMinValue(minNumbers(inputNumbers));
+        }
+    }
+}
