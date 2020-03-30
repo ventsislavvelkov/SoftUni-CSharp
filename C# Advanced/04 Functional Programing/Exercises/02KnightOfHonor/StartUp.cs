b54de1a1532da7250f249c0f4be3661f06824f36
Ventsislav Velkov <venci.velkov@gmail.com>
2020-03-30 18:26:05 +0300

    Homeworks

---
+++ C# Advanced/04 Functional Programing/Exercises/02KnightOfHonor/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace P02KnightOfHonor
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Action<string[]> printNames = names =>
+                Console.WriteLine("Sir " + string.Join(Environment.NewLine + "Sir ", names));
+
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            printNames(input);
+        }
+    }
+}
