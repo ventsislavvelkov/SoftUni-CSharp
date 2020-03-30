f0b496836491dfe0328c64c46a886e7794ac0bac
Ventsislav Velkov <venci.velkov@gmail.com>
2020-03-30 17:58:43 +0300

    HomeWorks

---
+++ C# Advanced/04 Functional Programing/Exercises/P01ActionPrint/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace P01ActionPrint
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Action<string[]> printNames = names =>
+                Console.WriteLine(string.Join(Environment.NewLine, names));
+
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            printNames(input);
+        }
+    }
+}
