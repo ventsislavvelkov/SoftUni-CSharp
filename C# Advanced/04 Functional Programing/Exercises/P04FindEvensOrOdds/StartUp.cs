42b62aa0e94cd6238a93df9c517fbcbd8221bd69
Ventsislav Velkov <venci.velkov@gmail.com>
2020-03-30 18:48:38 +0300

    Homeworks

---
+++ C# Advanced/04 Functional Programing/Exercises/P04FindEvensOrOdds/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04FindEvensOrOdds
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var bounds = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var lowerBound = bounds[0];
+            var upperBound = bounds[1];
+            var command = Console.ReadLine();
+            var numbers = new List<int>();
+
+            for (int i = lowerBound; i <= upperBound; i++)
+            {
+                numbers.Add(i);
+            }
+
+            Predicate<int> isOddNumber = x => x % 2 != 0;
+            Predicate<int> isEvenNumber = x => x % 2 == 0;
+
+            Action<List<int>> printNumbers = oddOrEvenNumbers =>
+                Console.WriteLine(string.Join(" ", oddOrEvenNumbers));
+
+            if (command == "odd")
+            {
+                printNumbers(numbers.Where(x => isOddNumber(x)).ToList());
+            }
+            else
+            {
+                printNumbers(numbers.Where(x => isEvenNumber(x)).ToList());
+            }
+        }
+    }
+}
