917cb2c749c66befaa9f67875f89a971b2101532
Ventsislav Velkov <venci.velkov@gmail.com>
2020-03-30 18:50:23 +0300

    Homeworks

---
+++ C# Advanced/04 Functional Programing/Exercises/P08CustomComparator/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace P08CustomComparator
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var inputNumbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int, int, int> sortFunc = (x, y)
+                => (x % 2 == 0 && y % 2 != 0) ? -1
+                : (x % 2 != 0 && y % 2 == 0) ? 1
+                : x > y ? 1 : x < y ? -1 : 0;
+
+            Array.Sort(inputNumbers, (x, y) => sortFunc(x, y));
+
+            Action<int[]> printNumbers = numbers =>
+                Console.WriteLine(string.Join(" ", numbers));
+
+            printNumbers(inputNumbers);
+        }
+    }
+}
