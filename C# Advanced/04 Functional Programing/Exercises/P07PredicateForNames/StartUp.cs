0c16f6fe128374f77558336633d5a3bf7630626a
Ventsislav Velkov <venci.velkov@gmail.com>
2020-03-30 18:50:05 +0300

    Homeworks

---
+++ C# Advanced/04 Functional Programing/Exercises/P07PredicateForNames/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace P07PredicateForNames
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var givenNameLenght = int.Parse(Console.ReadLine());
+            var inputNames = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Predicate<string> filterNames = name => name.Length <= givenNameLenght;
+
+            Action<string[]> printNames = names =>
+                Console.WriteLine(string.Join(Environment.NewLine, names));
+
+            printNames(inputNames.Where(x => filterNames(x)).ToArray());
+        }
+    }
+}
