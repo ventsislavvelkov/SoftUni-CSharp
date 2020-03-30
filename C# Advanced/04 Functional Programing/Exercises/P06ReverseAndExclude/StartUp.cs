c002ae1713bdb8214f6bbe14815d1aba66b1e2f4
Ventsislav Velkov <venci.velkov@gmail.com>
2020-03-30 18:49:46 +0300

    Homeworks

---
+++ C# Advanced/04 Functional Programing/Exercises/P06ReverseAndExclude/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace P06ReverseAndExclude
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var inputNumbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var givenInteger = int.Parse(Console.ReadLine());
+
+            Func<int[], int[]> reverseFunction = numbers =>
+            {
+                numbers = numbers.Reverse().ToArray();
+
+                return numbers;
+            };
+
+            Predicate<int> divisibleNumbers = x => x % givenInteger != 0;
+
+            Action<int[]> printNumbers = numbers =>
+                Console.WriteLine(string.Join(" ", numbers));
+
+            inputNumbers = reverseFunction(inputNumbers);
+            printNumbers(inputNumbers.Where(x => divisibleNumbers(x)).ToArray());
+        }
+    }
+}
