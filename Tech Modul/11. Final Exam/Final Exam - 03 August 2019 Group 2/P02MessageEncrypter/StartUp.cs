0578dae27e4aeb61dff374fc5340caa7ef533054
Ventsislav Velkov <venci.velkov@gmail.com>
2020-04-02 11:39:11 +0300

    FinalExam

---
+++ Tech Modul/11. Final Exam/Final Exam - 03 August 2019 Group 2/P02MessageEncrypter/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace P02MessageEncrypter
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var pattern =
+                @"(\*|@)(?<name>[A-Z][a-z]{2,})(\1):\s{1}\[(?<first>[A-Za-z])\]\|\[(?<second>[A-Za-z])\]\|\[(?<third>[A-Za-z])\]\|$";
+
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine();
+                var match = Regex.Match(input, pattern);
+                if (match.Success)
+                {
+                    var name = match.Groups["name"].Value;
+                    var first = char.Parse(match.Groups["first"].Value);
+                    var second = char.Parse(match.Groups["second"].Value);
+                    var third = char.Parse(match.Groups["third"].Value);
+
+                    Console.WriteLine($"{name}: {(int)first} {(int)second} {(int)third}");
+                }
+                else
+                {
+                    Console.WriteLine("Valid message not found!");
+                }
+
+            }
+        }
+    }
+}
