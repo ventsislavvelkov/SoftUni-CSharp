f7260e1b7aa36029970244896eebac7ab700ebbb
Ventsislav Velkov <venci.velkov@gmail.com>
2020-04-07 01:05:33 +0300

    Homework

---
+++ C# Advanced/07 Generics/Lab/P01BoxOfT/Box.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> element;
+        public int Count { get; private set; }
+        public T Element { get; set; }
+
+        public Box ()
+        {
+            this.element = new List<T>();
+        }
+
+        public void Add(T value)
+        {
+            this.element.Add(value);
+            this.Count++;
+        }
+
+        public T Remove()
+        {
+            T remove = this.element[this.Count - 1];
+            this.element.RemoveAt(this.Count-1);
+            this.Count--;
+
+            return remove; 
+        }
+    }
+}
---
+++ C# Advanced/07 Generics/Lab/P01BoxOfT/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using BoxOfT;
+
+namespace P01BoxOfT
+{
+    class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            Box<int> box = new Box<int>();
+            box.Add(1);
+            box.Add(2);
+            box.Add(3);
+            Console.WriteLine(box.Remove());
+            box.Add(4);
+            box.Add(5);
+            Console.WriteLine(box.Remove());
+
+
+        }
+    }
+}
