6943b65ac20386f92330fda6427a521fc99babc1
Ventsislav Velkov <venci.velkov@gmail.com>
2020-04-08 19:39:32 +0300

    Homework

---
+++ C# Advanced/07 Generics/Exercises/Threeuple/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Threeuple
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var first = Console.ReadLine().Split();
+            var fullName = $"{first[0]} {first[1]}";
+            var address = first[2];
+            var town = first[3];
+
+            var second = Console.ReadLine().Split();
+            var nameSecond = second[0];
+            var litresOfBeer = int.Parse(second[1]);
+            var drink = second[2];
+
+            var thurd = Console.ReadLine().Split();
+            var nameThurd = thurd[0];
+            var accountBalance = double.Parse(thurd[1]);
+            var bankName = thurd[2];
+
+            var firstThreeple = new Threeuple<string,string,string>(fullName,address,town);
+            var secondThreeple = new Threeuple<string,int,string>(nameSecond,litresOfBeer,drink);
+            var thurdThreeple = new Threeuple<string,double,string>(nameThurd,accountBalance,bankName);
+
+            Console.WriteLine(firstThreeple);
+            Console.WriteLine(secondThreeple);
+            Console.WriteLine(thurdThreeple);
+        }
+    }
+}
---
+++ C# Advanced/07 Generics/Exercises/Threeuple/Threeuple.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Threeuple
+{
+    class Threeuple<TFirst, TSecond, TThurd>
+    {
+        public Threeuple(TFirst first, TSecond second, TThurd thurd)
+        {
+            this.First = first;
+            this.Second = second;
+            this.Thurd = thurd;
+        }
+
+        public TFirst First { get; set; }
+        public TSecond Second { get; set; }
+        public TThurd Thurd { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.First} -> {this.Second} -> {this.Thurd}";
+        }
+    }
+}
