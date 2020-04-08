3ea61012f3a6ff25db16b5da08c87957f21bfe8d
Ventsislav Velkov <venci.velkov@gmail.com>
2020-04-08 11:45:27 +0300

    Homework

---
+++ C# Advanced/07 Generics/Lab/P03GenericScale/EqualityScale.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    public class EqualityScale<T>
+          where T:IComparable<T>
+    {
+        private T left;
+        private T right;
+
+        public EqualityScale(T left, T right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public bool AreEqual()
+        {
+            var value = this.left.CompareTo(this.right);
+
+            var result = false;
+
+            if (value != 0)
+            {
+                result = false;
+            }
+            else
+            {
+                result = true;
+            }
+
+            return result;
+        }
+
+    }
+}
+
