f8dbe9d934b319e23954940a49b4ca9815348972
Ventsislav Velkov <venci.velkov@gmail.com>
2020-04-09 01:45:10 +0300

    Homework

---
+++ C# Advanced/08 Iterators and Comparators/Lab/P02LibraryIterator/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book
+    {
+
+        public Book(string title, int year, params string[] autor)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Autors = autor.ToList();
+        }
+        public List<string> Autors { get; private set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+
+    }
+}
