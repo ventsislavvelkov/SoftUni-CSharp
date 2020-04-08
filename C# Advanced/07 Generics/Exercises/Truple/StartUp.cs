150dcd30c88967dfeba1e5a3b155f8996006b63b
Ventsislav Velkov <venci.velkov@gmail.com>
2020-04-08 16:24:55 +0300

    Homework

---
+++ C# Advanced/07 Generics/Exercises/Truple/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Truple
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var personInfo = Console.ReadLine().Split();
+            var fullName = $"{personInfo[0]} {personInfo[1]}";
+            var address = personInfo[2];
+
+            var nameAndBeer = Console.ReadLine().Split();
+            var name = nameAndBeer[0];
+            var beerAmount = int.Parse(nameAndBeer[1]);
+
+            var input = Console.ReadLine().Split();
+            var firstArgument = int.Parse(input[0]);
+            var secondArgument = double.Parse(input[1]);
+
+            var first = new Truple<string,string>(fullName,address);
+            var second = new Truple<string,int>(name,beerAmount);
+            var thurd = new Truple<int,double>(firstArgument,secondArgument);
+
+            Console.WriteLine(first);
+            Console.WriteLine(second);
+            Console.WriteLine(thurd);
+            
+
+        }
+    }
+}
---
+++ C# Advanced/07 Generics/Exercises/Truple/Truple.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Truple
+{
+    public class Truple<TFirst, TSecond>
+    {
+        public Truple(TFirst firstItem, TSecond secondItem)
+        {
+            this.FirstItem = firstItem;
+            this.SecondItem = secondItem;
+
+        }
+        public TFirst FirstItem { get; set; }
+        public TSecond SecondItem { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.FirstItem} -> {this.SecondItem}";
+        }
+    }
+}
