821386824baf3391096adee9b38e8cbd58bb0a1a
Ventsislav Velkov <venci.velkov@gmail.com>
2020-04-09 01:44:39 +0300

    Homework

---
+++ C# Advanced/08 Iterators and Comparators/Lab/P01Library/Library.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library( params Book[] name)
+        {
+            this.books = new List<Book>(name);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            // return this.books.GetEnumerator;
+
+            foreach (var book in books)
+            {
+                yield return book;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ C# Advanced/08 Iterators and Comparators/Lab/P01Library/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IteratorsAndComparators
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var bookOne = new Book("Animal Farm", 2003, "George Orwell");
+            var bookTwo = new Book("The Documents in the Case", 2002, "Dorothy Sayers", "Robert Eustace");
+            var bookThree = new Book("The Documents in the Case", 1930);
+
+            var libraryOne = new Library();
+            var libraryTwo = new Library(bookOne, bookTwo, bookThree);
+
+        }
+    }
+}
