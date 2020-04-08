60959f5ba15786e425f985d70203d7ed08d59e88
Ventsislav Velkov <venci.velkov@gmail.com>
2020-04-08 11:44:41 +0300

    Homework

---
+++ C# Advanced/07 Generics/Exercises/P01GenericBoxOfString/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01GenericBoxOfString
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var names =  new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = int.Parse(Console.ReadLine());
+                names.Add(input);
+            }
+            var box = new Box<int>(names);
+
+            var indexexToSwap = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            box.Swap(names, indexexToSwap[0], indexexToSwap[1]);
+            Console.WriteLine(box);
+        }
+    }
+}
---
+++ C# Advanced/07 Generics/Lab/P02GenericArrayCreator/ArrayCreator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace GenericArrayCreator
+{
+    public static class ArrayCreator
+    {
+        public static T[] Create<T>(int lenght, T value)
+        {
+            var arr = new T[lenght];
+
+            for (int i = 0; i < lenght; i++)
+            {
+                arr[i] = value;
+            }
+
+            return arr;
+        }
+    }
+}
