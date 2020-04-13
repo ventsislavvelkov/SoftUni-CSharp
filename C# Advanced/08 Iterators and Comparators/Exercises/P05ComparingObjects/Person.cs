16332bc3e0dc44817f86a23f349712666b136da6
Ventsislav Velkov <venci.velkov@gmail.com>
2020-04-13 21:20:15 +0300

    Homeworks

---
+++ C# Advanced/08 Iterators and Comparators/Exercises/P05ComparingObjects/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using Microsoft.VisualBasic;
+
+namespace P05ComparingObjects
+{
+    public class Person :IComparable<Person>
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Town { get; set; }
+
+        public Person(string name, int age, string town)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Town = town;
+        }
+
+        public int CompareTo([AllowNull] Person person)
+        {
+            int result = 1;
+
+            if (person != null)
+            {
+                result = this.Name.CompareTo(person.Name);
+
+                if (result == 0)
+                {
+                    result = this.Age.CompareTo(person.Age);
+
+                    if (result == 0)
+                    {
+                        result = this.Town.CompareTo(person.Town);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ C# Advanced/08 Iterators and Comparators/Exercises/P05ComparingObjects/StartUp.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05ComparingObjects
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var people = new List<Person>();
+            var input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                var personinfo = input.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                var name = personinfo[0];
+                var age = int.Parse(personinfo[1]);
+                var town = personinfo[2];
+
+                var person = new Person(name, age, town);
+
+                people.Add(person);
+            }
+
+            var matchesCount = 0;
+            var n = int.Parse(Console.ReadLine());
+
+            var personToCompare = people[n - 1];
+
+            foreach (var person in people)
+            {
+                if (personToCompare.CompareTo(person) == 0)
+                {
+                    matchesCount++;
+                }
+            }
+
+            if (matchesCount <= 1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                var notMatchesCount = people.Count - matchesCount;
+                Console.WriteLine($"{matchesCount} {notMatchesCount} {people.Count}   "); 
+            }
+        }
+    }
+}
