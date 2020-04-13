3a6e3517e7e8179ebb232224c486497a6eb4f009
Ventsislav Velkov <venci.velkov@gmail.com>
2020-04-13 20:46:03 +0300

    Homework

---
+++ C# Advanced/08 Iterators and Comparators/Exercises/P04Froggy/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace P04Froggy
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var stones = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var lake = new Lake(stones);
+
+            Console.WriteLine(string.Join(", ", lake));
+
+        }
+    }
+}
