dd3340abc6ec35aaf8ccc8f08c6ca3a73417e8db
Ventsislav Velkov <venci.velkov@gmail.com>
2020-04-14 16:43:19 +0300

    Homeworks

---
+++ C# Advanced/08 Iterators and Comparators/Exercises/P01ListyIterator/ListyIterator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private int currentIndex;
+
+        private List<T> store;
+
+
+        public ListyIterator()
+        {
+            this.store = new List<T>();
+            currentIndex = 0;
+        }
+
+        public ListyIterator(List<T> store)
+        {
+            this.store = new List<T>(store);
+            currentIndex = 0;
+        }
+
+        public void Add(List<T> elements)
+        {
+            this.store.AddRange(elements);
+        }
+
+        public bool Move()
+        {
+
+            if (this.HasNext())
+            {
+                currentIndex++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Print()
+        {
+            if (this.store.Any())
+            {
+                Console.WriteLine(this.store[currentIndex]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operation!");
+            }
+        }
+
+        public bool HasNext()
+        {
+            if (currentIndex < this.store.Count-1)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- C# Advanced/08 Iterators and Comparators/Exercises/P01ListyIterator/StartUp.cs
+++ C# Advanced/08 Iterators and Comparators/Exercises/P01ListyIterator/StartUp.cs
@@ -4,10 +4,35 @@ using System.Linq;
 
 namespace ListyIterator
 {
-    class StartUp
+    public class StartUp
     {
         static void Main(string[] args)
         {
+            var create = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Skip(1)
+                .ToList();
+            var listyiterator = new ListyIterator<string>(create);
+            
+
+            var input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                switch (input)
+                {
+                    case "Move":
+                        Console.WriteLine(listyiterator.Move());
+                        break;
+                    case "Print":
+                        listyiterator.Print();
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(listyiterator.HasNext());
+                        break;
+                }
+            }
+
+
 
         }
     }
