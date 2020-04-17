ab0b84326c2fa9272160581ff213382b73c240b7
Ventsislav Velkov <venci.velkov@gmail.com>
2020-04-17 12:10:34 +0300

    Homework

--- C# OOP/01 Working with Abstraction/Exercise/P01_RawData/Engine.cs
+++ C# OOP/01 Working with Abstraction/Exercise/P01_RawData/Engine.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Win32.SafeHandles;
 
 namespace P01_RawData
 {
-    class Engine
+    public class Engine
     {
+        public Engine(int speed, int power)
+        {
+            this.Speed = speed;
+            this.Power = power;
+
+        }
+        public int  Speed { get; set; }
+        public int  Power { get; set; }
+
     }
 }
