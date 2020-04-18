88093c96e8524214abd9826340f238ae8fd427e4
Ventsislav Velkov <venci.velkov@gmail.com>
2020-04-18 21:21:32 +0300

    Homeworks

---
+++ C# OOP/02 Inheritance/Exercise/Person/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+
+        }
+
+        public string Name { get; }
+
+        public  int Age
+        {
+            get => this.age;
+            set
+            {
+                if (value > 0)
+                {
+                    this.age = value;
+                }
+
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {this.Name}, Age: {this.Age}";
+        }
+    }
+}
