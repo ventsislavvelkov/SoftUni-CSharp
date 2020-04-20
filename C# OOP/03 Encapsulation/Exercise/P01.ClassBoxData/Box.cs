febdb6e72fd6f4aaac0a4fa5421b2415c8a95dd9
Ventsislav Velkov <venci.velkov@gmail.com>
2020-04-21 00:29:45 +0300

    Homework

---
+++ C# OOP/03 Encapsulation/Exercise/P01.ClassBoxData/Box.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01.ClassBoxData
+{
+    public class Box
+    {
+        private const int SIDE_MIN_VALUE = 0;
+        private const string SIDE_ERROR_MESSAGE = "{0} cannot be zero or negative.";
+
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length
+        {
+            get => this.length;
+            private set
+            {
+                this.ValidateSide(value, nameof(this.Length));
+
+                this.length = value;
+            }
+        }
+
+        public double Width
+        {
+            get => this.width;
+            private set
+            {
+                this.ValidateSide(value, nameof(this.Width));
+
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get => this.height;
+            private set
+            {
+                this.ValidateSide(value, nameof(this.Height));
+
+                this.height = value;
+            }
+        }
+
+        public double CalculateSurfaceArea()
+        {
+            var surfaceArea = (2 * this.Length * this.Width) +
+                              (2 * this.Length * this.Height) +
+                              (2 * this.Width * this.Height);
+            return surfaceArea;
+        }
+
+        public double CalculateLateralSurfaceArea()
+        {
+            var lateraSurfaceArea = (2 * this.Length * this.Height) +
+                                    (2 * this.Width * this.Height);
+
+            return lateraSurfaceArea;
+        }
+
+        public double CalculateVolume()
+        {
+            var volume = this.Length * this.Width * this.Height;
+
+            return volume;
+        }
+        private void ValidateSide(double value, string sideName)
+        {
+            if (value <= SIDE_MIN_VALUE)
+            {
+                throw new ArgumentException(String.Format(SIDE_ERROR_MESSAGE, sideName));
+            }
+        }
+    }
+}
---
+++ C# OOP/03 Encapsulation/Exercise/P01.ClassBoxData/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P01.ClassBoxData
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var length = double.Parse(Console.ReadLine());
+            var width = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                var box = new Box(length, width, height);
+
+                Console.WriteLine($"Surface Area - {box.CalculateSurfaceArea():f2}");
+                Console.WriteLine($"Lateral Surface Area - {box.CalculateLateralSurfaceArea():f2}");
+                Console.WriteLine($"Volume - {box.CalculateVolume():f2}");
+
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+          
+        }
+    }
+}
