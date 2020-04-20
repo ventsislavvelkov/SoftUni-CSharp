2d1a9d1b9d7d1f7cd7f37b62d913431e65f03780
Ventsislav Velkov <venci.velkov@gmail.com>
2020-04-21 00:30:13 +0300

    Homework

---
+++ C# OOP/03 Encapsulation/Exercise/P02.AnimalFarm/AnimalFarm/Models/Chicken.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AnimalFarm.Models
+{
+    public class Chicken
+    {
+        private const int MinAge = 0;
+        private const int MaxAge = 15;
+        private const string INVALID_NAME = "Name cannot be empty.";
+        private const string INVALID_AGE = "Age should be between {0} and {1}.";
+
+        private string name;
+        private int age;
+
+        public Chicken(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(INVALID_NAME);
+                }
+
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            get => this.age;
+            private set
+            {
+                if (MinAge > value || value > MaxAge   )
+                {
+                    throw new ArgumentException(String.Format(INVALID_AGE, MinAge, MaxAge));
+                }
+
+                this.age = value;
+            }
+        }
+
+        public double ProductPerDay => this.CalculateProductPerDay();
+
+        private double CalculateProductPerDay()
+        {
+            switch (this.Age)
+            {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                {
+                    return 1.5;
+                }
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                    return 2;
+                case 8:
+                case 9:
+                case 10:
+                case 11:
+                    return 1;
+                default:
+                    return 0.75;
+            }
+        }
+    }
+}
