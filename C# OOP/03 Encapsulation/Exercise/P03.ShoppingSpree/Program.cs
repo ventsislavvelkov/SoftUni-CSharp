b18e910a1a1da7f43d5b2e3caa0683602b2e1bb0
Ventsislav Velkov <venci.velkov@gmail.com>
2020-04-22 21:12:41 +0300

    Homework

---
+++ C# OOP/03 Encapsulation/Exercise/P03.ShoppingSpree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P03.ShoppingSpree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
