88aeb57ce1e3f41a09274a1d85c5439e58b718b5
Ventsislav Velkov <venci.velkov@gmail.com>
2020-04-25 19:17:23 +0300

    Homework

---
+++ C# OOP/03 Encapsulation/Exercise/P04.PizzaCalories/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P04.PizzaCalories
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
