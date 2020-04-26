c6c35f6579a9dc1c69b4837ab4737e29e34752ed
Ventsislav Velkov <venci.velkov@gmail.com>
2020-04-27 00:20:20 +0300

    Homework

--- C# OOP/04 Interfaces and Abstraction/Exercise/P01.DefineAnInterfaceIPerson/Citizen.cs
+++ C# OOP/04 Interfaces and Abstraction/Exercise/P01.DefineAnInterfaceIPerson/Citizen.cs
@@ -4,14 +4,18 @@ using System.Text;
 
 namespace PersonInfo
 {
-    public class Citizen : IPerson
+    public class Citizen : IPerson, IIdentifiable, IBirthable
     {
-        public Citizen(string name, int age)
+        public Citizen(string name, int age, string id, string birthdate)
         {
             this.Name = name;
             this.Age = age;
+            this.Id = id;
+            this.Birthdate = birthdate;
         }
         public string Name { get; set; }
         public int Age { get ; set ; }
+        public string Id { get ; set ; }
+        public string Birthdate { get ; set ; }
     }
 }
--- C# OOP/04 Interfaces and Abstraction/Exercise/P01.DefineAnInterfaceIPerson/StartUp.cs
+++ C# OOP/04 Interfaces and Abstraction/Exercise/P01.DefineAnInterfaceIPerson/StartUp.cs
@@ -9,9 +9,13 @@ namespace PersonInfo
         {
             string name = Console.ReadLine();
             int age = int.Parse(Console.ReadLine());
-            IPerson person = new Citizen(name, age);
-            Console.WriteLine(person.Name);
-            Console.WriteLine(person.Age);
+            string id = Console.ReadLine();
+            string birthdate = Console.ReadLine();
+            IIdentifiable identifiable = new Citizen(name, age, id, birthdate);
+            IBirthable birthable = new Citizen(name, age, id, birthdate);
+            Console.WriteLine(identifiable.Id);
+            Console.WriteLine(birthable.Birthdate);
+
 
 
         }
