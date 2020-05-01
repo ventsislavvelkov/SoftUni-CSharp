fa2d3c2e7842b47482c51eb53c625be1f87189e8
Ventsislav Velkov <venci.velkov@gmail.com>
2020-05-02 00:00:25 +0300

    Homework

---
+++ Entity Framework Core/Fetching Resultsets with ADO.NET/Exersice/P09.IncreaseAgeStoredProcedure/Queries.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P09.IncreaseAgeStoredProcedure
+{
+    class Queries
+    {
+        public static string CreateProcedure = @"CREATE OR ALTER PROC usp_GetOlder @id INT
+                                                 AS
+                                                 UPDATE Minions
+                                                 SET Age += 1
+                                                 WHERE Id = @Id";
+
+        public static string SelectMinionNameAndAge = "SELECT Name, Age FROM Minions WHERE Id = @Id";
+    }
+}
---
+++ Entity Framework Core/Fetching Resultsets with ADO.NET/Exersice/P09.IncreaseAgeStoredProcedure/StartUp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace P09.IncreaseAgeStoredProcedure
+{
+    class StartUp
+    {
+        private const string ProcedureName = "usp_GetOlder";
+
+        public static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection(string.Format(Configuration.ConnectionString));
+
+            connection.Open();
+
+            using (connection)
+            {
+                int minionId = int.Parse(Console.ReadLine());
+
+                // Create procedure
+                using SqlCommand createCommand = new SqlCommand(Queries.CreateProcedure, connection);
+                createCommand.ExecuteNonQuery();
+
+                // Exec procedure
+                using SqlCommand execCommand = new SqlCommand(ProcedureName, connection);
+
+                execCommand.CommandType = CommandType.StoredProcedure;
+                execCommand.Parameters.AddWithValue("@Id", minionId);
+                execCommand.ExecuteNonQuery();
+
+                // Create reader to show result after procedure executes
+                using SqlCommand selectCommand = new SqlCommand(Queries.SelectMinionNameAndAge, connection);
+                selectCommand.Parameters.AddWithValue("@Id", minionId);
+
+                using SqlDataReader reader = selectCommand.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    string minionName = (string)reader["Name"];
+                    int minionAge = (int)reader["Age"];
+
+                    Console.WriteLine($"{minionName} - {minionAge} years old");
+                }
+            }
+        }
+    }
+}
