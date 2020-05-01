0d74ffd4eeaab571ee36d5b6c0e347b7246b88e7
Ventsislav Velkov <venci.velkov@gmail.com>
2020-05-01 23:44:15 +0300

    Homework

---
+++ Entity Framework Core/Fetching Resultsets with ADO.NET/Exersice/P06.RemoveVillain/Queries.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P06.RemoveVillain
+{
+    class Queries
+    {
+        public static string TakeVillainId = "SELECT Name FROM Villains WHERE Id = @villainId";
+
+        public static string DeleteVillainFromMinionsVillains = @"DELETE FROM MinionsVillains 
+                                                                        WHERE VillainId = @villainId";
+
+        public static string DeleteVillainFromVillains = @"DELETE FROM Villains
+                                                                 WHERE Id = @villainId";
+    }
+}
