6dc5e8db490945e05b32f3915f8001996a4a02d7
Ventsislav Velkov <venci.velkov@gmail.com>
2020-05-01 23:38:46 +0300

    Homework

---
+++ Entity Framework Core/Fetching Resultsets with ADO.NET/Exersice/P05.ChangeTownNamesCasing/Configuration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P05.ChangeTownNamesCasing
+{
+    class Configuration
+    {
+        public const string ConnectionString = @"Server=.\SQLEXPRESS;Database=MinionsDB;Integrated Security=true";
+    }
+}
