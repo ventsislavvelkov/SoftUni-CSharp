e890727afebe779f2392f41915fc3276c5030b18
Ventsislav Velkov <venci.velkov@gmail.com>
2020-05-01 21:52:05 +0300

    Homework

---
+++ Entity Framework Core/Fetching Resultsets with ADO.NET/Exersice/P02.VillainNames/Queries.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P02.VillainNames
+{
+    class Queries
+    {
+        public static string VillainNames = @"SELECT [Name], 
+                                            COUNT(mv.MinionId) AS [MinionsCount]
+                                       FROM Villains AS v
+                                            JOIN MinionsVillains AS mv ON v.Id = mv.VillainId
+                                   GROUP BY [Name]
+                                     HAVING COUNT(mv.MinionId) > 3
+                                   ORDER BY COUNT(mv.MinionId) DESC";
+    }
+}
---
+++ Entity Framework Core/Fetching Resultsets with ADO.NET/Exersice/P02.VillainNames/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace P02.VillainNames
+{
+    class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection(String.Format(Configuration.ConnectionString, "master"));
+
+            connection.Open();
+
+            using (connection)
+            {
+                using SqlCommand sqlCommand = new SqlCommand(Queries.VillainNames, connection);
+
+                try
+                {
+                    using SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                    while (sqlDataReader.Read())
+                    {
+                        string villainName = (string)sqlDataReader["Name"];
+                        int minionsCount = (int)sqlDataReader["MinionsCount"];
+
+                        Console.WriteLine($"{villainName} - {minionsCount}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Invalid operation: {ex.Message}");
+                }
+            }
+        }
+    }
+}
+
