0734689076d1c8e29d220a6fd01b27c9dd2a92e0
Ventsislav Velkov <venci.velkov@gmail.com>
2020-05-01 23:48:06 +0300

    Homework

---
+++ Entity Framework Core/Fetching Resultsets with ADO.NET/Exersice/P07.PrintAllMinionNames/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace P07.PrintAllMinionNames
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection(String.Format(Configuration.ConnectionString));
+
+            connection.Open();
+
+            using (connection)
+            {
+                using SqlCommand sqlCommand = new SqlCommand(Queries.TakeMinionNames, connection);
+
+                using SqlDataReader sqlReader = sqlCommand.ExecuteReader();
+
+                List<string> originalNames = new List<string>(sqlReader.FieldCount);
+
+                while (sqlReader.Read())
+                {
+                    string name = (string)sqlReader["Name"];
+                    originalNames.Add(name);
+                }
+
+                Console.WriteLine($"Original order: {Environment.NewLine + string.Join(Environment.NewLine, originalNames)}");
+
+                Console.WriteLine("----------------");
+
+                Console.WriteLine("New order:");
+
+                while (originalNames.Count != 0)
+                {
+                    Console.WriteLine(originalNames[0]);
+                    originalNames.RemoveAt(0);
+
+                    if (originalNames.Count == 0)
+                    {
+                        break;
+                    }
+
+                    Console.WriteLine(originalNames.Last());
+                    originalNames.RemoveAt(originalNames.Count - 1);
+                }
+            }
+        }
+    }
+}
