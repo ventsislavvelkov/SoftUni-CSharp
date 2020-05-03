08cd0c9904fc00447a0aff5ff52de45c93bc9431
Ventsislav Velkov <venci.velkov@gmail.com>
2020-05-03 22:55:55 +0300

    Homework

---
+++ Databases Basics - MS SQL Server/03 Built-in Functions/03FindFirstNamesOfAllEmployess.sql
@@ -0,0 +1,4 @@
+SELECT FirstName
+  FROM Employees
+ WHERE DepartmentID IN(3, 10) 
+   AND (DATEPART(YEAR, HireDate) BETWEEN 1995 AND 2005)
---
+++ Databases Basics - MS SQL Server/03 Built-in Functions/04FindAllEmployeesExceptEngineers.sql
@@ -0,0 +1,4 @@
+SELECT FirstName,
+       LastName
+  FROM Employees
+ WHERE JobTitle NOT LIKE '%engineer%'
