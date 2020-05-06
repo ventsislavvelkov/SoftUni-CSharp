2a3fbd67f444d96db03ec868d01d208612e402bd
Ventsislav Velkov <venci.velkov@gmail.com>
2020-05-06 22:47:04 +0300

    Homework

---
+++ Databases Basics - MS SQL Server/06 Subqueries and Join/01EmployeeAddress.sql
@@ -0,0 +1,7 @@
+SELECT TOP 5 e.EmployeeID, 
+             e.JobTitle, 
+             a.AddressID, 
+             a.AddressText
+FROM Employees AS e
+     JOIN Addresses AS a ON e.AddressID = a.AddressID
+ORDER BY a.AddressID
---
+++ Databases Basics - MS SQL Server/06 Subqueries and Join/02AddressesWithTowns.sql
@@ -0,0 +1,9 @@
+SELECT TOP 50 e.FirstName, 
+              e.LastName, 
+              t.[Name] AS Town, 
+              a.AddressText
+FROM Employees AS e
+     JOIN Addresses AS a ON e.AddressID = a.AddressID
+     JOIN Towns AS t ON a.TownID = t.TownID
+ORDER BY e.FirstName, 
+         e.LastName
