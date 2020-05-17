65827ecdeee00741749bff708e26e7684ea8cb8e
Ventsislav <venci.velkov@gmail.com>
2020-05-18 01:26:00 +0300

    Homework

--- QA Automation/02 Selenium Basic/SeleniumBasic/AutomationPracticeRegistration/AutomationPracticeTest.cs
+++ QA Automation/02 Selenium Basic/SeleniumBasic/AutomationPracticeRegistration/AutomationPracticeTest.cs
@@ -1,8 +1,64 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace AutomationPracticeRegistration
 {
     public class AutomationPracticeTest
     {
+        private IWebDriver _driver;
+        private WebDriverWait _wait;
+
+        private string email = "veloman@abv.bg";
+
+        [SetUp]
+        public void SetUp()
+        {
+            _driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))
+            {
+                Url = "http://automationpractice.com/index.php"
+            };
+
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(2));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+           _driver.Quit();
+        }
+
+        [Test]
+        public void CreateAccount()
+        {
+            var clickSignInBtn = _wait.Until<IWebElement>(d =>
+                d.FindElement(By.XPath("//*[@id=\"header\"]/div[2]/div/div/nav/div[1]/a")));
+
+            clickSignInBtn.Click();
+
+            var createAccount = _wait.Until<IWebElement>(d=>
+                d.FindElement(By.XPath("//*[@id=\"email_create\"]")));
+
+            createAccount.SendKeys(email);
+
+            var clickCreateAnAccountBtn = _wait.Until<IWebElement>(d =>
+                d.FindElement(By.XPath("//*[@id=\"SubmitCreate\"]")));
+
+            clickCreateAnAccountBtn.Click();
+
+            var checkMailIfIsCorrect =  _wait.Until<bool>(d => 
+                d.FindElement(By.Id("email")).GetProperty("value") == email);
+
+            var mailField = _driver.FindElement(By.Id("email"));
+            var curValue = mailField.GetProperty("value");
+
+            Assert.AreEqual(email, curValue);
+
+        }
+
     }
 }
