d62da7f43398dae4e7e23135779a45f56a3eb53d
Ventsislav <venci.velkov@gmail.com>
2020-05-17 20:49:58 +0300

    Homework

--- QA Automation/02 Selenium Basic/SeleniumBasic/GoogleSearch/GoogleSearchTest.cs
+++ QA Automation/02 Selenium Basic/SeleniumBasic/GoogleSearch/GoogleSearchTest.cs
@@ -1,9 +1,58 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+using System.Reflection;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace GoogleSearch
 {
+    [TestFixture]
     public class GoogleSearchTest
     {
+        private IWebDriver _driver;
+        private WebDriverWait _wait;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            _driver.Url = "http://www.google.com";
+
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(2));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+         //   _driver.Quit();
+        }
+
+        [Test]
+
+        public void SearchSeleniumWebsite()
+        {
+            var mainPageSearchBox = _wait.Until<IWebElement>(d=>
+                d.FindElement(By.XPath("//*[@id='tsf']/div[2]/div[1]/div[1]/div/div[2]/input")));
+
+            mainPageSearchBox.SendKeys("selenium");
+
+            var mainPageSearchBtn = _wait.Until<IWebElement>(d =>
+                d.FindElement(By.XPath("//*[@id='tsf']/div[2]/div[1]/div[3]/center/input[1]")));
+
+            mainPageSearchBtn.Click();
+
+            var firstResult = _wait.Until<IWebElement>(d => 
+                d.FindElement(By.XPath("//*[@id='rso']/div[1]//a")));
+           
+            firstResult.Click();
+
+            var seleniumTitle = _wait.Until<IWebElement>(d =>
+                d.FindElement(By.XPath("/html/head/title")));
+
+            Assert.AreEqual(seleniumTitle, "Selenium - Web Browser Automation");
+        }
 
     }
 }
---
+++ QA Automation/02 Selenium Basic/SeleniumBasic/QaAutomation/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace QaAutomation
+{
+    public class Class1
+    {
+    }
+}
