4438627dfaff03b78dd580438e06ec7a1bb80f69
Ventsislav <venci.velkov@gmail.com>
2020-05-18 00:50:15 +0300

    Homework

--- QA Automation/02 Selenium Basic/SeleniumBasic/QaAutomation/SoftUniTest.cs
+++ QA Automation/02 Selenium Basic/SeleniumBasic/QaAutomation/SoftUniTest.cs
@@ -27,32 +27,46 @@ namespace QaAutomation
         [TearDown]
         public void TearDown()
         {
-          //  _driver.Quit();
+            
+
+            foreach (var window in _driver.WindowHandles)
+            {
+                _driver.SwitchTo().Window(window);
+                _driver.Close();
+            }
+
+            _driver.Quit();
         }
 
         [Test]
 
         public void HomePageNavigationBar()
         {
-            var menuBtnClick = _wait.Until<IWebElement>(d =>
-                d.FindElement(By.XPath("//*[@id=\"header-nav\"]/div[2]/ul/li[1]/a/span/i")));
-            menuBtnClick.Click();           
-            
-            var coursesBtnClick = _wait.Until<IWebElement>(d =>
-                d.FindElement(By.XPath("//*[@id=\"header-nav\"]/div[1]/ul/li[2]/a/span")));
-            coursesBtnClick.Click();
 
-            var courseQaAutomationClick = _wait.Until<IWebElement>(d =>
-                d.FindElement(By.CssSelector(
-                    "#header-nav > div.toggle-nav.toggle-holder.toggle-active > ul > li.nav-item.dropdown-item.nav-item-active.dropdown-active.open > div > div > div.row.no-margin-offset.courses-and-modules-wrapper > div.col-md-8.open-courses-wrapper.open-courses-background > div > div:nth-child(2) > ul:nth-child(4) > div:nth-child(1) > ul > li > a")));
-            courseQaAutomationClick.Click();
+            var homePageSearchBtnClick = _wait.Until<IWebElement>(d =>
+                d.FindElement(By.XPath("//*[@id=\"search-icon-container\"]/a/span/i")));
+            homePageSearchBtnClick.Click();
+
+            var mainPageSearchBox = _wait.Until<IWebElement>(d =>
+                d.FindElement(By.XPath("//*[@id=\"search-input\"]")));
+
+            mainPageSearchBox.SendKeys("Qa");
+
+            var searchBtnClick = _wait.Until<IWebElement>(d =>
+                d.FindElement(By.XPath("//*[@id=\"search-dropdown\"]/div/form/div[1]/button")));
+            searchBtnClick.Click();
+
+            var QaCoursesLink = _wait.Until<IWebElement>(d =>
+                d.FindElement(By.XPath("//*[@id=\"fast-track-instance-results\"]/ul/li[1]/div/a")));
+            QaCoursesLink.Click();
+
+            var allOpenPages = _driver.WindowHandles;
+            _driver.SwitchTo().Window(allOpenPages[1]);
 
             var headingТagH1Contains = _wait.Until<IWebElement>(d =>
-                d.FindElement(By.XPath("/html/body/div[2]/header/h1")));
+                d.FindElement(By.XPath("/html/body/div[2]/header/h1"))).Text;
 
             Assert.AreEqual("QA Automation - май 2020", headingТagH1Contains);
         }
-
-
     }
 }
