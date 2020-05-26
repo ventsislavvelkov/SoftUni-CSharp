6c3f756c04378130f5303ed12b79560ff3b221fb
Ventsislav <venci.velkov@gmail.com>
2020-05-26 21:29:26 +0300

    Homework

---
+++ Entity Framework Core/04 Code-First/HospitalDatabase/HospitalDatabase/Data/HospitalContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace P01_HospitalDatabase.Data
+{
+   public  class HospitalContext : DbContext
+    {
+        public HospitalContext()
+        {
+            
+        }
+    }
+}
