e988e72ecae1f7a3198ffeb4c85c3677f2151353
Ventsislav <venci.velkov@gmail.com>
2020-05-30 20:52:08 +0300

    Homework

---
+++ Entity Framework Core/05 Entity Relations/P01_StudentSystem/P01_StudentSystem/Data/Models/Resiurces.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_StudentSystem.Data.Models
+{
+   public  class Resiurces
+    {
+    }
+}
---
+++ Entity Framework Core/05 Entity Relations/P01_StudentSystem/P01_StudentSystem/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P01_StudentSystem
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
