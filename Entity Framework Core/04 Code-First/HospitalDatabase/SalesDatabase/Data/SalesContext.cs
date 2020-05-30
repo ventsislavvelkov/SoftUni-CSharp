dea9d37e0fe903927804ac921a0ae9695abcca2b
Ventsislav <venci.velkov@gmail.com>
2020-05-30 16:15:40 +0300

    Homework

--- Entity Framework Core/04 Code-First/HospitalDatabase/SalesDatabase/Data/SalesContext.cs
+++ Entity Framework Core/04 Code-First/HospitalDatabase/SalesDatabase/Data/SalesContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.ExpressionTranslators.Internal;
 using P03_SalesDatabase.Data.Models;
 
 namespace P03_SalesDatabase.Data
@@ -40,7 +41,85 @@ namespace P03_SalesDatabase.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.HasKey(c => c.CustomerId);
+
+                entity
+                    .Property(c => c.Name)
+                    .HasMaxLength(50)
+                    .IsRequired(true)
+                    .IsUnicode(true);
+
+                entity
+                    .Property(c => c.Email)
+                    .HasMaxLength(80)
+                    .IsRequired(true)
+                    .IsUnicode(false);
+
+                entity
+                    .Property(c => c.CreditCardNumber)
+                    .HasMaxLength(20)
+                    .IsRequired(true)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.HasKey(p => p.ProductId);
+
+                entity
+                    .Property(p => p.Name)
+                    .HasMaxLength(50)
+                   
+                    .IsRequired(true);
+
+                entity
+                    .Property(p => p.Quantity)
+                    .IsRequired(true);
+
+                entity
+                    .Property(p => p.Price)
+                    .IsRequired(true); 
+
+            });
+
+            modelBuilder.Entity<Store>(entity =>
+            {
+                entity.HasKey(s => s.StoreId);
+
+                entity
+                    .Property(c => c.Name)
+                    .HasMaxLength(80)
+                    .IsRequired(true)
+                    .IsUnicode(true);
+            });
+
+            modelBuilder.Entity<Sale>(entity =>
+            {
+                entity.HasKey(s => s.SaleId);
+
+                entity
+                    .Property(s => s.Date)
+                    .IsRequired(true)
+                    .HasColumnType("DATETIME2");
+
+                entity
+                    .HasOne(s => s.Product)
+                    .WithMany(p => p.Sales)
+                    .HasForeignKey(s => s.ProductId);
+
+                entity
+                    .HasOne(s => s.Customer)
+                    .WithMany(c => c.Sales)
+                    .HasForeignKey(s => s.CustomerId);
+
+                entity
+                    .HasOne(s => s.Store)
+                    .WithMany(s => s.Sales)
+                    .HasForeignKey(s => s.StoreId);
+
+            });
         }
     }
 }
