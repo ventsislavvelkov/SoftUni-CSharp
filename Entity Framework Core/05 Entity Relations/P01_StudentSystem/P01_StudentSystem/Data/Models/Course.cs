0d840659bb07aedded75efa634bee71bcb2c3efc
Ventsislav <venci.velkov@gmail.com>
2020-05-30 22:23:49 +0300

    Homework

--- Entity Framework Core/05 Entity Relations/P01_StudentSystem/P01_StudentSystem/Data/Models/Course.cs
+++ Entity Framework Core/05 Entity Relations/P01_StudentSystem/P01_StudentSystem/Data/Models/Course.cs
@@ -1,10 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace P01_StudentSystem.Data.Models
 {
     public class Course
     {
+        public int CourseId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public decimal Price { get; set; }
     }
 }
