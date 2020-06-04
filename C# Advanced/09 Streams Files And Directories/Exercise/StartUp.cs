14216c499d7b81af28bbd17e6d5b9b070c155a01
Ventsislav <venci.velkov@gmail.com>
2020-06-04 23:59:55 +0300

    Homework

---
+++ C# Advanced/09 Streams Files And Directories/Exercise/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace P01EvenLines
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var reader = new StreamReader(@"files\input.txt");
+
+            using (reader)
+            {
+                var counter = 0;
+                var line = reader.ReadLine();
+
+                var symbolsToReplace = new string[] { "-", ",", ".", "!", "?" };
+
+                using (var writer = new StreamWriter(@"files\output.txt"))
+                {
+                    while (line != null)
+                    {
+                        if (counter % 2 == 0)
+                        {
+                            foreach (var symbol in symbolsToReplace)
+                            {
+                                if (line.Contains(symbol))
+                                {
+                                    line = line.Replace(symbol, "@");
+                                }
+                            }
+
+                            line = string.Join(" ", line.Split(" ").Reverse());
+                            writer.WriteLine(line);
+                        }
+
+                        counter++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
