2fd3603f29649e2d666c96723c8f8f47e214bade
Ventsislav <venci.velkov@gmail.com>
2020-06-07 19:16:14 +0300

    Homework

---
+++ QA Automation/04 Best Practices - Design Patterns/Homework/Homework/Pages/Google/SearchResultPage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework.Pages.Google
+{
+    class SearchResultPage
+    {
+    }
+}
---
+++ QA Automation/04 Best Practices - Design Patterns/Homework/Homework/Tests/AutomationPractice/PractiseTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework.Tests.AutomationPractice
+{
+    class PractiseTest
+    {
+    }
+}
