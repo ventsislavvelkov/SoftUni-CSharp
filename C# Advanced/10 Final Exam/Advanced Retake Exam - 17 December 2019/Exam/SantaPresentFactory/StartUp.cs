632e71a456d668af6681ae116cc0613943617a1e
Ventsislav <venci.velkov@gmail.com>
2020-06-11 21:59:33 +0300

    Homework

--- C# Advanced/10 Final Exam/Advanced Retake Exam - 17 December 2019/Exam/SantaPresentFactory/StartUp.cs
+++ C# Advanced/10 Final Exam/Advanced Retake Exam - 17 December 2019/Exam/SantaPresentFactory/StartUp.cs
@@ -1,12 +1,82 @@
 ﻿using System;
+using System.Linq;
 
 namespace SantaPresentFactory
 {
-    class Program
+    public class StartUp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var presents = int.Parse(Console.ReadLine());
+            var n = int.Parse(Console.ReadLine());
+
+            var santaRow = 0;
+            var santaCol = 0;
+
+            var niceKids = 0;
+
+            var matrix = new char[n, n];
+
+            for (var row = 0; row < matrix.GetLength(0); row++)
+            {
+                var input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+
+                for (var col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+
+                    if (input[col] == 'S')
+                    {
+                        santaCol = col;
+                        santaRow = row;
+                    }
+                    else if (input[col] == 'V')
+                    {
+                        niceKids++;
+                    }
+                }
+            }
+
+
+            //while (niceKids > 0)
+            //{
+            //    var direction = Console.ReadLine();
+            //    var santaNewRow = santaRow;
+            //    var santaNewCol = santaCol;
+
+            //    switch (direction)
+            //    {
+            //        case "up":
+            //            santaNewRow--;
+            //            break;
+            //        case "down":
+            //            santaNewRow++;
+            //            break;
+            //        case "right":
+            //            santaNewCol++;
+            //            break;
+            //        case "left":
+            //            santaNewCol--;
+            //            break;
+            //    }
+
+            //}
+
+            PrintMatrix(matrix);
+
+        }
+
+        private static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
