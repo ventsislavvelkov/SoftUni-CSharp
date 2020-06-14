391f564dec7b6704b083fa7dcc9fa0e2f4a6f099
Ventsislav <venci.velkov@gmail.com>
2020-06-14 23:57:19 +0300

    Homework

---
+++ C# Advanced/10 Final Exam/Advanced Retake Exam - 17 December 2019/Exam/Christmas/Bag.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Christmas
+{
+    public class Bag
+    {
+        private List<Present> data;
+
+        public Bag()
+        {
+            this.data = new List<Present>();
+        }
+
+        public Bag(string color, int capacity)
+            : this()
+        {
+            this.Color = color;
+            this.Capacity = capacity;
+        }
+
+        public string Color { get; set; }
+        public int Capacity { get; set; }
+
+        public void Add(Present present)
+        {
+            if (this.data.Count < this.Capacity)
+            {
+                this.data.Add(present);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            var isRemoved = false;
+
+            if (this.data.Any(p => p.Name == name))
+            {
+                var present = this.data.FirstOrDefault(p => p.Name == name);
+                this.data.Remove(present);
+                isRemoved = true;
+            }
+
+            return isRemoved;
+        }
+
+        public Present GetHeaviestPresent()
+        {
+            var present = this.data.OrderByDescending(p => p.Weight).FirstOrDefault();
+            return present;
+        }
+
+        public Present GetPresent(string name)
+        {
+            var present = this.data.FirstOrDefault(p => p.Name == name);
+
+            return present;
+        }
+
+        public int Count => this.data.Count;
+
+        public string Report()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"{Color} bag contains:");
+
+            foreach (var p in data)
+            {
+                sb.AppendLine($"Present {p.Name} for a {p.Gender}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+
+    }
+}
---
+++ C# Advanced/10 Final Exam/Advanced Retake Exam - 17 December 2019/Exam/Christmas/Present.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Christmas
+{
+   public class Present
+    {
+        public Present(string name, double weight, string gender)
+        {
+            this.Name = name;
+            this.Weight = weight;
+            this.Gender = gender;
+        }
+
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public string Gender { get; set; }
+
+        public override string ToString()
+        {
+            return $"Present {Name} ({Weight}) for a {Gender}";
+        }
+    }
+}
