01d09650286cefc6ee21ff82917821731948384f
Ventsislav <venci.velkov@gmail.com>
2020-06-17 15:12:59 +0300

    Homework

---
+++ C# Advanced/10 Final Exam/Advanced Exam - 24 Feb 2019/exam/heroes/Item.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heroes
+{
+    public class Item
+    {
+        public Item(int strength, int ability, int intelligence)
+        {
+            this.Strength = strength;
+            this.Ability = ability;
+            this.Intelligence = intelligence;
+        }
+        public int Strength { get; set; }
+        public int Ability { get; set; }
+        public int Intelligence { get; set; }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine("Item:")
+                .AppendLine($"  * Strength: {this.Strength}")
+                .AppendLine($"  * Ability: {this.Ability}")
+                .AppendLine($"  * Intelligence: {this.Intelligence}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
