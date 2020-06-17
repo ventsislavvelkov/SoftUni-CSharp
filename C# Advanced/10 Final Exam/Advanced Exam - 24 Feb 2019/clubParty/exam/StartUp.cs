02f5a3ea5da21f08d314cb07e59a811311fa02f3
Ventsislav <venci.velkov@gmail.com>
2020-06-17 11:15:56 +0300

    Homework

---
+++ C# Advanced/10 Final Exam/Advanced Exam - 24 Feb 2019/clubParty/exam/StartUp.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace exam
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var capacity = int.Parse(Console.ReadLine());
+            var consoleInput = Console.ReadLine().Split();
+            var input = new Stack<string>(consoleInput);
+            var hall = new Queue<char>();
+            var reservation = new List<int>();
+
+            while (input.Any())
+            {
+                var currChar = char.Parse(input.Pop());
+
+                if (char.IsLetter(currChar))
+                {
+                    hall.Enqueue(currChar);
+                }
+                else
+                {
+                    if (hall.Any())
+                    {
+                        var sumOfReservation = 0;
+                       
+                        if (reservation.Any())
+                        {
+                            foreach (var r in reservation)
+                            {
+                                sumOfReservation += r;
+                            }
+
+                            if (sumOfReservation < capacity)
+                            {
+                                reservation.Add(currChar);
+                            }
+                            else
+                            {
+                                Console.WriteLine($"{hall.Dequeue()} -> {string.Join(", ", reservation)}");
+                                reservation.Clear();
+                            }
+                        }
+
+
+                    }
+                }
+            }
+
+
+
+        }
+    }
+}
