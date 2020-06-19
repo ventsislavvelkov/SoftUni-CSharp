36a5313aa9ed69b42d09b0bcf9a9fa4b36c97db1
Ventsislav <venci.velkov@gmail.com>
2020-06-19 12:56:23 +0300

    Homework

--- C# Advanced/10 Final Exam/Advanced Exam - 24 Feb 2019/exam/heroes/Hero.cs
+++ C# Advanced/10 Final Exam/Advanced Exam - 24 Feb 2019/exam/heroes/Hero.cs
@@ -6,13 +6,24 @@ namespace Heroes
 {
     public class Hero
     {
-        public Hero()
+        public Hero(string name, int level, Item item)
         {
-            
+            this.Name = name;
+            this.Level = level;
+            this.Item = item;
         }
         public string Name { get; set; }
         public int Level { get; set; }
         public Item Item { get; set; }
 
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"Hero: {this.Name} - {this.Level}lvl")
+                .AppendLine($"{this.Item.ToString()}");
+
+            return sb.ToString().TrimEnd();
+        }
     }
 }
---
+++ C# Advanced/10 Final Exam/Advanced Exam - 24 Feb 2019/exam/heroes/HeroRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Heroes
+{
+    public class HeroRepository
+    {
+        private List<Hero> data;
+
+        public HeroRepository()
+        {
+            this.data = new List<Hero>();
+        }
+
+        public int Count => this.data.Count;
+        public void Add(Hero hero)
+        {
+            this.data.Add(hero);
+        }
+
+        public void Remove(string name)
+        {
+            this.data.RemoveAll(h => h.Name == name);
+        }
+
+        public Hero GetHeroWithHighestStrength()
+        {
+            return this.data.OrderByDescending(h => h.Item.Strength).FirstOrDefault();
+        }
+
+        public Hero GetHeroWithHighestAbility()
+        {
+            return this.data.OrderByDescending(h => h.Item.Ability).FirstOrDefault();
+        }
+
+        public Hero GetHeroWithHighestIntelligence()
+        {
+            return this.data.OrderByDescending(h => h.Item.Intelligence).FirstOrDefault();
+        }
+
+        public override string ToString()
+        {
+            return string.Join(" ", data);
+        }
+    }
+}
