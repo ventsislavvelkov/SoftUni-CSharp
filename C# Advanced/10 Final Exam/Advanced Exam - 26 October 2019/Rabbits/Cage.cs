b1d1bf4d198efcc62ae03e730d381b2ac3a9e168
Ventsislav <venci.velkov@gmail.com>
2020-06-21 23:16:13 +0300

    Homework

---
+++ C# Advanced/10 Final Exam/Advanced Exam - 26 October 2019/Rabbits/Cage.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+
+namespace Rabbits
+{
+    public class Cage
+    {
+        private List<Rabbit> data;
+
+        public Cage(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            data = new List<Rabbit>();
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+
+        public void Add(Rabbit rabbit)
+        {
+            if (this.Capacity > 0)
+            {
+                data.Add(rabbit);
+            }
+        }
+
+        public bool RemoveRabbit(string name)
+        {
+           var rabbitName = data.FirstOrDefault(r => r.Name == name);
+
+           return data.Remove(rabbitName);
+        }
+
+        public void RemoveSpecies(string species)
+        {
+            data.RemoveAll(r => r.Species == species);
+        }
+
+        public Rabbit SellRabbit(string name)
+        {
+            var sellRabbit = data.FirstOrDefault(r => r.Name == name);
+            sellRabbit.Available = false;
+            return sellRabbit;
+        }
+
+        public Rabbit[] SellRabbitsBySpecies(string species)
+        {
+            var rabbitBySpecies = data.Where(r=>r.Species == species);
+            
+            foreach (var r in rabbitBySpecies)
+            {
+                r.Available = false;
+            }
+
+            return rabbitBySpecies.ToArray();
+        }
+
+        public int Count => data.Count;
+
+
+    }
+}
---
+++ C# Advanced/10 Final Exam/Advanced Exam - 26 October 2019/Rabbits/Rabbit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rabbits
+{
+    public class Rabbit
+    {
+        public Rabbit()
+        {
+            this.Available = true;
+        }
+
+        public Rabbit(string name, string species)
+            : base()
+        {
+            this.Name = name;
+            this.Species = species;
+        }
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public bool Available { get; set; }
+
+        public override string ToString()
+        {
+            return $"Rabbit ({this.Species}): {this.Name}";
+        }
+    }
+}
