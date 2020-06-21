afb2e0e67e9b6fb42be31d513f667d80961accb7
Ventsislav <venci.velkov@gmail.com>
2020-06-21 21:59:59 +0300

    Homework

--- C# Advanced/10 Final Exam/Advanced Exam - 26 October 2019/BookWorm/Program.cs
+++ C# Advanced/10 Final Exam/Advanced Exam - 26 October 2019/BookWorm/Program.cs
@@ -6,7 +6,86 @@ namespace BookWorm
     {
         static void Main(string[] args)
         {
-            
+            var initialString = Console.ReadLine().ToCharArray();
+            var rowColMatrix = int.Parse(Console.ReadLine());
+
+            var matrix = new char[rowColMatrix, rowColMatrix];
+
+            var playerCol = 0;
+            var playerRow = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var input = Console.ReadLine();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                    if (input[col] == 'P')
+                    {
+                        playerCol = col;
+                        playerRow = row;
+                    }
+                }
+            }
+
+            var command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                
+            }
+            switch (command)
+            {
+                case "up":
+                    playerRow--;
+
+                    break;
+                case "down":
+                    playerRow++;
+                    break;
+                case "right":
+                    playerCol++;
+                    break;
+                case "left":
+                    playerCol--;
+                    break;
+            }
+
+            PrintMatrix(matrix);
+        }
+
+        private static bool IsValidCell(char[,] matrix, int rows, int cols)
+        {
+            var isValidCell = false;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (rows >= 0 && rows < matrix.GetLength(0) &&
+                        cols >= 0 && cols < matrix.GetLength(1))
+                    {
+                        isValidCell = true;
+                        break;
+                    }
+                }
+            }
+
+            return isValidCell;
+        }
+
+        private static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
