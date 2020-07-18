1d810ccd721e06c845c9aa586707cfeec712fe77
Ventsislav <venci.velkov@gmail.com>
2020-07-19 01:07:07 +0300

    Homework

--- Entity Framework Core/07 Advanced Querying/BookShop/StartUp.cs
+++ Entity Framework Core/07 Advanced Querying/BookShop/StartUp.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Text;
+using BookShop.Models.Enums;
 
 namespace BookShop
 {
@@ -11,16 +12,18 @@ namespace BookShop
     {
         public static void Main()
         {
-            using var db = new BookShopContext(); 
+            using BookShopContext db = new BookShopContext(); 
            // DbInitializer.ResetDatabase(db);
 
-            var input = Console.ReadLine();
+            //var input = Console.ReadLine();
 
-            var result = GetBooksByAgeRestriction(db, input);
+            var result = GetBooksByPrice(db);
 
             Console.WriteLine(result);
 
         }
+
+        // 2. Age Restriction
         public static string GetBooksByAgeRestriction(BookShopContext context, string command)
         {
             var result = context.Books
@@ -31,6 +34,41 @@ namespace BookShop
                 .ToList();
 
             return String.Join(Environment.NewLine, result);
+        }
+
+        //3. Golden Books
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            var goldenBook = context.Books
+                .Where(b => b.EditionType == EditionType.Gold && b.Copies < 5000)
+                .OrderBy(b => b.BookId)
+                .Select(b => b.Title)
+                .ToList();
+
+            return String.Join(Environment.NewLine, goldenBook);
+        }
+
+        // 4. Books by Price
+        public static string GetBooksByPrice(BookShopContext context)
+        {
+            var result = context.Books
+                .Where(b => b.Price > 40)
+                .Select(b => new
+                {
+                    Title = b.Title,
+                    Price = b.Price
+                })
+                .OrderByDescending(b => b.Price)
+                .ToList();
+
+            var sb = new StringBuilder();
+
+            foreach (var r in result)
+            {
+                sb.AppendLine($"{r.Title} - ${r.Price}");
+            }
+
+            return sb.ToString().TrimEnd();
         }
     }
 }
