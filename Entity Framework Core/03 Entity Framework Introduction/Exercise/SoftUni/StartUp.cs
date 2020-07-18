2071307aa0521112ffce6b62279049153b5fd74e
Ventsislav <venci.velkov@gmail.com>
2020-07-18 22:45:17 +0300

    Homework

--- Entity Framework Core/03 Entity Framework Introduction/Exercise/SoftUni/StartUp.cs
+++ Entity Framework Core/03 Entity Framework Introduction/Exercise/SoftUni/StartUp.cs
@@ -14,7 +14,7 @@ namespace SoftUni
         {
             var context = new SoftUniContext();
 
-            var result = IncreaseSalaries(context);
+            var result = GetEmployeesByFirstNameStartingWithSa(context);
             Console.WriteLine(result);
 
 
@@ -335,5 +335,25 @@ namespace SoftUni
 
             return sb.ToString().TrimEnd();
         }
+
+        public static string GetEmployeesByFirstNameStartingWithSa(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Where(e => e.FirstName.StartsWith("Sa"))
+                .OrderBy(e => e.FirstName)
+                .ThenBy(e => e.LastName)
+                .ToList();
+
+            var sb = new StringBuilder();
+
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} - {e.JobTitle} - (${e.Salary:f2})");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
     }
+
+
 }
