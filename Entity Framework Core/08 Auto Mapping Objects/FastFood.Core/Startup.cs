537144673dd9791b43eed10c64fc825029e527df
Ventsislav <venci.velkov@gmail.com>
2020-07-21 21:16:23 +0300

    Homework

--- Entity Framework Core/08 Auto Mapping Objects/FastFood.Core/Startup.cs
+++ Entity Framework Core/08 Auto Mapping Objects/FastFood.Core/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -29,6 +30,8 @@ namespace FastFood.Core
             services.AddDbContext<FastFoodContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddAutoMapper(typeof(Startup));
 
             services.AddControllersWithViews();
         }
