81591adf8d1e01fcd0a5c73ec02e1aeeecba39e2
Ventsislav <venci.velkov@gmail.com>
2020-07-24 22:51:15 +0300

    Homework

---
+++ Entity Framework Core/09 JSON Processing/ProductShop/ProductShop/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AutoMapper;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            ProductShopContext db = new ProductShopContext();
+
+            ResetDatabase(db);
+
+        }
+
+        private static void ResetDatabase(ProductShopContext db)
+        {
+            db.Database.EnsureDeleted();
+            Console.WriteLine("Database was Successfully deleted!");
+
+            db.Database.EnsureCreated();
+            Console.WriteLine("Database was Successfully created!");
+        }
+    }
+}
