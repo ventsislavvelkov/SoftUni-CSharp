53dce041a017bfe9aa77c8dea7cf6423c8995588
Ventsislav <venci.velkov@gmail.com>
2020-07-29 00:10:55 +0300

    Homework

---
+++ Entity Framework Core/10 XML Processing/CarDealer/Dto/Export/ExportCarWithTheirPartsDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer.Dto.Export
+{
+    [XmlType("car")]
+  public  class ExportCarWithTheirPartsDto
+    {
+        [XmlAttribute("make")]
+        public string Make { get; set; }
+        [XmlAttribute("model")]
+        public string Model { get; set; }
+        [XmlAttribute("travelled-distance")]
+        public long TravelledDistance { get; set; }
+        [XmlArray("parts")]
+        public ExportPartsDto[] Parts { get; set; }
+    }
+
+  [XmlType("part")]
+  public class ExportPartsDto
+  {
+      [XmlAttribute("name")]
+        public string Name { get; set; }
+        [XmlAttribute("price")]
+        public decimal Price { get; set; }
+
+  }
+}
---
+++ Entity Framework Core/10 XML Processing/CarDealer/Dto/Export/ExportSalesByCustomerDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer.Dto.Export
+{
+    [XmlType("customer")]
+   public class ExportSalesByCustomerDto
+    {
+        [XmlAttribute("full-name")]
+        public string FullName { get; set; }
+        [XmlAttribute("bought-cars")]
+        public int  BoughtCars { get; set; }
+        [XmlAttribute("spent-money")]
+        public decimal SpentMoney { get; set; }
+    }
+}
