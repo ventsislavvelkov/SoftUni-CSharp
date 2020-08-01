e30dfd83f0d46520c841e578474633dcd58044d3
Ventsislav <venci.velkov@gmail.com>
2020-08-01 14:27:53 +0300

    Homework

---
+++ Entity Framework Core/12 Exams/13 Dec 19/BookShop/DataProcessor/ExportDto/ExportOldestBooksDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace BookShop.DataProcessor.ExportDto
+{
+    [XmlType("Book")]
+   public class ExportOldestBooksDto
+    {
+        [XmlAttribute("Pages")]
+        public int Pages { get; set; }
+        [XmlElement("Name")]
+        public string Name { get; set; }
+        [XmlElement("Date")]
+        public string PublishedOn { get; set; }
+
+
+    }
+}
