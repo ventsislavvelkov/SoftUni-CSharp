da375dd780141837d6837aaf07cc74c61b16f6c0
Ventsislav Velkov <venci.velkov@gmail.com>
2020-08-02 12:52:05 +0300

    Homework

---
+++ Entity Framework Core/12 Exams/07 Dec 19/TeisterMask/Data/TeisterMaskContext.cs
@@ -0,0 +1,25 @@
+﻿namespace TeisterMask.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class TeisterMaskContext : DbContext
+    {
+        public TeisterMaskContext() { }
+
+        public TeisterMaskContext(DbContextOptions options)
+            : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
