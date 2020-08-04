6abec4588c4e7a6e1d3286b7743931e614eb76da
Ventsislav <venci.velkov@gmail.com>
2020-08-04 23:38:31 +0300

    Homework

---
+++ Entity Framework Core/12 Exams/18 Apr 19/MusicHub/Data/Models/Album.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusicHub.Data.Models
+{
+    public class Album
+    {
+    }
+}
---
+++ Entity Framework Core/12 Exams/18 Apr 19/MusicHub/Data/Models/Song.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using MusicHub.Data.Models.Enums;
+
+namespace MusicHub.Data.Models
+{
+   public class Song
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; }
+        [Required]
+        public TimeSpan Duration { get; set; }
+
+        public DateTime CreateOn { get; set; }
+
+        public Genre Genre { get; set; }
+        [ForeignKey(nameof(Album))]
+        public int AlbumId { get; set; }
+
+        public virtual Album Album { get; set; }
+        [ForeignKey(nameof(Writer))]
+        public int WriteId { get; set; }
+
+        public virtual Writer Writer { get; set; }
+
+        public decimal Price { get; set; }
+
+        public virtual ICollection<SongPerformer> SongPerformers { get; set; }
+        = new HashSet<SongPerformer>();
+    }
+}
