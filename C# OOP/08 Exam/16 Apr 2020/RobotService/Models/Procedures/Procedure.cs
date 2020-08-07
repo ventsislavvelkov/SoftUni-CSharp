4a0bba25377704ba68d9f97c2e5e57edc2a4ed65
Ventsislav <venci.velkov@gmail.com>
2020-08-07 23:11:16 +0300

    Homework

---
+++ C# OOP/08 Exam/16 Apr 2020/RobotService/Models/Procedures/Procedure.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RobotService.Models.Procedures.Contracts;
+using RobotService.Models.Robots.Contracts;
+
+namespace RobotService.Models.Procedures
+{
+   public abstract class Procedure : IProcedure
+   {
+       private List<IRobot> robots;
+
+       public Procedure()
+       {
+           this.robots = new List<IRobot>();
+       }
+
+       public IList<IRobot> Robots => this.robots;
+
+        public string History()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"{this.GetType().Name}");
+
+            foreach (IRobot robot in this.Robots)
+            {
+                sb.AppendLine($"{robot}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public void DoService(IRobot robot, int procedureTime)
+        {
+            
+        }
+    }
+}
---
+++ C# OOP/08 Exam/16 Apr 2020/RobotService/Models/Robots/Robot.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RobotService.Models.Robots.Contracts;
+using RobotService.Utilities.Messages;
+
+namespace RobotService.Models.Robots
+{
+  public abstract class Robot : IRobot
+    {
+        private int happiness;
+        private int energy;
+        private string owner = "Service";
+
+        public Robot(string name, int energy, int happiness, int procedureTime)
+        {
+            this.Name = name;
+            this.Energy = energy;
+            this.Happiness = happiness;
+            this.ProcedureTime = procedureTime;
+
+        }
+        public string Name { get; }
+
+        public int Happiness
+        {
+            get => this.happiness;
+
+            set
+            {
+                if (value < 0 || value > 100)
+                {
+                    throw  new ArgumentException(ExceptionMessages.InvalidHappiness);
+                }
+
+                this.happiness = value;
+            }
+
+        }
+
+        public int Energy
+        {
+            get => this.energy;
+
+            set
+            {
+                if (value < 0 || value > 100)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidEnergy);
+                }
+
+                this.energy = value;
+            }
+        }
+
+        public int ProcedureTime { get; set; }
+
+        public string Owner
+        {
+            get => this.owner;
+            set => this.owner = value;
+        }
+
+        public bool IsBought { get; set; } = false;
+        public bool IsChipped { get; set; } = false;
+        public bool IsChecked { get; set; } = false;
+
+        public override string ToString()
+        {
+            return $" Robot type: {this.GetType().Name} - {this.Name} - Happiness: {this.Happiness} - Energy: {this.Energy}";
+        }
+    }
+}
